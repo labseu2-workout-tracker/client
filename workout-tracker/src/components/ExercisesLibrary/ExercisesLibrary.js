3407f038012795302d927437175f1ac502a32d99
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 22:45:41 +0200

    import showSingleExercise action in ExerciseLibrary

--- workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
+++ workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AllExercises from "./AllExercises";
 import SingleExercise from "./SingleExercise";
-import { fetchExercises, showMuscleGroup, paginate } from "../../store/actions";
+import { fetchExercises, showMuscleGroup, paginate, showSingleExercise } from "../../store/actions";
 import { connect } from "react-redux";
 
 class ExerciseLibrary extends React.Component {
@@ -44,5 +44,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { fetchExercises, showMuscleGroup, paginate }
+  { fetchExercises, showMuscleGroup, paginate, showSingleExercise }
 )(ExerciseLibrary);
