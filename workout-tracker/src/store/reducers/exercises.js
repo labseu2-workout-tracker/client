c41006a880defe52ba0f236713a2121cb2df43be
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 18:36:40 +0200

    created exercises reducer with action for FETCH_EXERCISES and added copyOfExercises redux state

--- workout-tracker/src/store/reducers/exercises.js
+++ workout-tracker/src/store/reducers/exercises.js
@@ -1,3 +1,24 @@
 const initialState = {
   exercises: null,
+  copyOfExercises: null
 };
+
+const exercises = (state = initialState, action) => {
+  switch (action.type) {
+    case types.FETCH_EXERCISES:
+      const filterExercisesWithoutRating = action.payload.filter(
+        exercise => exercise.exercise_ratings !== "n/a"
+      );
+
+      return {
+        ...state,
+        exercises: filterExercisesWithoutRating,
+        copyOfExercises: filterExercisesWithoutRating
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default exercises;
