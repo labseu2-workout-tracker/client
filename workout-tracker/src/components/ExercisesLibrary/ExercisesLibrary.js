ae5246f582aa0b1a984d25bef5ecb1cb0e0da4a9
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 18:52:12 +0200

    import React and create Class ExerciseLibrary

--- workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
+++ workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class ExerciseLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default ExerciseLibrary;
