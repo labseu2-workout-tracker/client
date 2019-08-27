a7d1ed7bcfe96aa6d5982a5b97d20675b01e9a3b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 18:48:17 +0200

    create action dispatcher for fetchExercises

--- workout-tracker/src/store/actions/index.js
+++ workout-tracker/src/store/actions/index.js
@@ -4,3 +4,14 @@ export const FETCH_EXERCISES = 'FETCH_EXERCISES';
 
 const exercises = 'http://localhost:5000/exercises';
 // adress get's changed later
+
+export const fetchExercises = () => dispatch => {
+  return axios.get(exercises)
+    .then(res => {
+      debugger
+      dispatch({ type: FETCH_EXERCISES, exercises: res.data });
+    })
+    .catch(err => {
+     debugger
+    })
+};
