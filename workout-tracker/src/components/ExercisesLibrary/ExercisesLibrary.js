8094d5ad425e59df569a9c4a0609c0875fd4fed5
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 20:16:46 +0200

    import showMuscleGroup action in ExerciseLibrary.js

--- workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
+++ workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import AllExercises from './AllExercises';
-import { fetchExercises } from '../../store/actions';
-import { connect } from 'react-redux';
+import React from "react";
+import AllExercises from "./AllExercises";
+import { fetchExercises, showMuscleGroup } from "../../store/actions";
+import { connect } from "react-redux";
 
 class ExerciseLibrary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
 
   componentDidMount = () => {
-  this.props.fetchExercises();
+    this.props.fetchExercises();
   };
 
-  render() { 
-    return ( 
-      <AllExercises 
-      exercises={this.props.exercises}
-      showMuscleGroup={(e) =>
-         this.props.showMuscleGroup(e.target.textContent)}  />
-     );
+  render() {
+    return (
+      <AllExercises
+        exercises={this.props.exercises}
+        showMuscleGroup={e =>
+           this.props.showMuscleGroup(e.target.textContent)}
+      />
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-  exercises: state.exercises.exercises,
+    exercises: state.exercises.exercises
   };
 };
- 
-export default connect(mapStateToProps, { fetchExercises })(ExerciseLibrary);
+
+export default connect(
+  mapStateToProps,
+  { fetchExercises,
+  showMuscleGroup }
+)(ExerciseLibrary);
