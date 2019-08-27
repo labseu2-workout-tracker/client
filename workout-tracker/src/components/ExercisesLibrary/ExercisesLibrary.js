1a2eb6eb49054773768f157bb4a5a03c909cf87a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 18:53:30 +0200

    import connect from react-redux and connect it to Class ExerciseLibrary

--- workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
+++ workout-tracker/src/components/ExercisesLibrary/ExercisesLibrary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class ExerciseLibrary extends Component {
   constructor(props) {
@@ -10,4 +11,4 @@ class ExerciseLibrary extends Component {
   }
 }
  
-export default ExerciseLibrary;
+export default connect(mapStateToProps, null)(ExerciseLibrary);
