04c880e72490069cc465124e1e7b6b50ce42cae0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 19:09:35 +0200

    added div and map method for getting all exercises to component AllExercises

--- workout-tracker/src/components/ExercisesLibrary/AllExercises.js
+++ workout-tracker/src/components/ExercisesLibrary/AllExercises.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const AllExercises = (props) => {
-  return (  );
+  return ( 
+    <div className="all-exercise">
+    {props.exercises ? (props.exercises.map((exercise, index) => {
+      return <div key={index}>
+        <p onClick={() =>
+           props.getExercise(exercise.id)}>{exercise.exercise_name}</p>
+        <button>Add</button>
+      </div>
+    })) : null}
+  </div>
+    );
 }
  
 export default AllExercises;
