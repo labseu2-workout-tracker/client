f5672fdb006a1adb3958b6ded99f95e5ca836a58
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-27 19:29:56 +0200

    added comments in actions index file

--- workout-tracker/src/store/actions/index.js
+++ workout-tracker/src/store/actions/index.js
@@ -6,12 +6,13 @@ const exercises = 'http://localhost:5000/exercises';
 // adress get's changed later
 
 export const fetchExercises = () => dispatch => {
+  // type LOADING needs to be added (also for the redux state) 
+
   return axios.get(exercises)
     .then(res => {
-      debugger
       dispatch({ type: FETCH_EXERCISES, exercises: res.data });
     })
     .catch(err => {
-     debugger
-    })
+   // type ERROR needs to be added (also for the redux state)
+    });
 };
