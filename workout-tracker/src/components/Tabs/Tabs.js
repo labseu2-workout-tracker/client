0b5930c22b260b3084f1417109768048890ecc67
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-28 18:36:02 +0200

    added key to all Links in Tabs.js

--- workout-tracker/src/components/Tabs/Tabs.js
+++ workout-tracker/src/components/Tabs/Tabs.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const Tabs = () => {
   const tabs = ["tracker", "workout", "settings", "notifications"];
 
-  return tabs.map(tab => (
-    <Link to={`/${tab}`}>{tab}</Link>
+  return tabs.map((tab, index) => (
+    <Link
+    key={index}
+    to={`/${tab}`}>{tab}</Link>
   ));
 };
 
