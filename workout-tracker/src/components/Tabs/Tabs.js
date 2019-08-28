42ebbc24f75e1b6d18b6f7f41f6d20ce5b3c28d2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-28 16:55:56 +0200

    added map method which loops through the tabs array and displays the different tabs + added onClick method for action call

--- workout-tracker/src/components/Tabs/Tabs.js
+++ workout-tracker/src/components/Tabs/Tabs.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 const Tabs = () => {
-  const tabs = ['Tracker', 'Workout', 'Settings', 'Notifications'];
-  
-  return ( 
-    
-   );
-}
- 
+  const tabs = ["Tracker", "Workout", "Settings", "Notifications"];
+
+  return tabs.map(tab => (
+    <button onClick={e => props.showCategory(e.target.textContent)}>
+      {tab}
+    </button>
+  ));
+};
+
 export default connect(null)(Tabs);
