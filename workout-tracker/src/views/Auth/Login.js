5e6105e75125a998ab25092bf83e909a9aba978d
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-29 22:37:08 +0400

    ft: login file created with imports

---
+++ workout-tracker/src/views/Auth/Login.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import { required, length, email } from '../../utils/validators';
+import Auth from './Auth';
+
