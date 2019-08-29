13409c19612c413a6cd7ec4f81df7881c8b7d8e0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 14:28:23 +0200

    created navButtons array

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const MainNavBar = () => {
+  const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
   return ( 
-    <div>This is the MainNavBar copmonent</div>
    );
 }
  
