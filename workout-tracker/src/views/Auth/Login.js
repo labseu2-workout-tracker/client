b833e677a388c0766be5c55d8b176032151a2df8
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-29 22:38:16 +0400

    ft: created class component and initialized login state

--- workout-tracker/src/views/Auth/Login.js
+++ workout-tracker/src/views/Auth/Login.js
@@ -5,3 +5,28 @@ import Button from '../../components/Button/Button';
 import { required, length, email } from '../../utils/validators';
 import Auth from './Auth';
 
+class Login extends Component {
+  state = {
+    loginForm: {
+      email: {
+        value: '',
+        valid: false,
+        touched: false,
+        validators: [required, email]
+      },
+      password: {
+        value: '',
+        valid: false,
+        touched: false,
+        validators: [required, length({ min: 5 })]
+      },
+      formIsValid: false
+    }
+  };
+
+  render() {
+    return ()
+  }
+}
+
+export default Login;
