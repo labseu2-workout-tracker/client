6687520023155ab650c26634c77a574917045262
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 18:06:07 +0200

    created variable StyledContactPage

--- workout-tracker/src/views/ContactPage/ContactPage.js
+++ workout-tracker/src/views/ContactPage/ContactPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '../Header/Header';
 import styled from 'styled-components';
 
+const StyledContactPage = styled.div`
+`;
+
 class ContactPage extends React.Component {
   constructor(props) {
     super(props);
