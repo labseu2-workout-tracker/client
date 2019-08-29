54b673627ba243d3706b8fc8c931a065bdd4b784
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 17:22:50 +0200

    import styled components in MainNavBar.js

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
 
 const MainNavBar = () => {
   const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
