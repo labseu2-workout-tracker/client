743bb1ebbc42ebcc3457a0a78919c7ccce56bc25
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 11:32:16 +0200

    bug fix in Tabs.js and App.js

--- workout-tracker/src/App.js
+++ workout-tracker/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tabs from './components/Tabs/Tabs';
-import ExercisesLibrary from './components/Exercises/ExercisesLibrary';
-import UserPage from './components/MainUserComponents/Tracker/UserPage';
+import ExercisesLibrary from './views/ExerciseLibrary/ExercisesLibrary';
+import UserPage from './views/UserPage/UserPage';
 import { Route } from 'react-router-dom';
 import './App.css';
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Tabs />
-        <Route path={'/workout'} component={ExercisesLibrary} />
+        <Route path={'/My Workouts'} component={ExercisesLibrary} />
         <Route path={'/tracker'} component={UserPage} />
     </div>
   );
