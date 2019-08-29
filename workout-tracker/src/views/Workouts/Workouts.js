248755208bc121e2d83958edcb1d74e8bbee0a22
Talent Antonio <antonio.talenty@gmail.com>
2019-08-29 14:50:39 +0100

    map over workout

--- workout-tracker/src/views/Workouts/Workouts.js
+++ workout-tracker/src/views/Workouts/Workouts.js
@@ -4,7 +4,14 @@ import { connect } from 'react-redux';
 const Workouts = () => {
   return ( 
     <div>
-      
+      {/* array- mappin over each workout */}
+      {this.props.workouts.map(workout =>{
+        return <div> 
+          <h2>{workout.workout_name}</h2>
+          <p>{workout.workout_description}</p>
+          <img src={workout.image_url} alt="workout" />
+        </div> 
+      })}
     </div>
    );
 }
