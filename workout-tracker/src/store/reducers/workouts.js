2a534ea25fb573896ba03edb91a1ee4613581e83
Talent Antonio <antonio.talenty@gmail.com>
2019-08-29 15:18:30 +0100

    workout reducer

--- workout-tracker/src/store/reducers/workouts.js
+++ workout-tracker/src/store/reducers/workouts.js
@@ -5,5 +5,19 @@ const initialState = {
   
 };
 
+const workouts = (state = initialState, action) => {
+    switch (action.type) {
+      case type.FETCH_WORKOUTS:
+        
+        return {
+          ...state,
+          workouts: action.workouts,
+        };
+  
+      
+      default:
+        return state;
+    }
+  };
 
 export default workouts;
