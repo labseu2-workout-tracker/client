a9ca9d7b010c7c8628b513d5b060363d7988e1e4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 14:54:13 +0200

    start of bugfix in Tabs.js: deleted string Tracker in tabs array

--- workout-tracker/src/views/Tabs/Tabs.js
+++ workout-tracker/src/views/Tabs/Tabs.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 const Tabs = () => {
-  const tabs = ["Tracker", "My Workouts", "Settings", "Notifications", 'History'];
+  const tabs = [ "My Workouts", "Settings", "Notifications", 'History'];
 
-  return tabs.map((tab, index) => (
+  return <div>
+    {tabs.map((tab, index) => 
     <Link
     key={index}
-    to={`/${tab}`}>{tab}</Link>
-  ));
+    to={`/${tab}`}>{tab}</Link>)}
+     </div>
 };
 
 export default Tabs;
