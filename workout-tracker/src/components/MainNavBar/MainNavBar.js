055e37b7aedfc89c581787be6ea95600a156c00d
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 16:33:13 +0200

    bugfix: added key with index param in map method in file MainNavBar.js

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const MainNavBar = () => {
   const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
-  return ( 
+  return (
     <nav>
-      {navButtons.map(button => <NavLink to={`/${button}`}>{button}</NavLink>)}
+      {navButtons.map((button, index) => (
+        <NavLink key={index} to={`/${button}`}>
+          {button}
+        </NavLink>
+      ))}
     </nav>
-   );
-}
- 
+  );
+};
+
 export default MainNavBar;
