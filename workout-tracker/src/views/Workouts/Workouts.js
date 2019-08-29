e2c33dc1211b4ce37e68a7cde486e352ad35a6d9
Talent Antonio <antonio.talenty@gmail.com>
2019-08-29 15:05:14 +0100

    if statement redux stage workouts

--- workout-tracker/src/views/Workouts/Workouts.js
+++ workout-tracker/src/views/Workouts/Workouts.js
@@ -13,13 +13,14 @@ class Workouts  extends React.Component {
     return(
       <div>
       {/* array- mappin over each workout */}
-      {this.props.workouts.map(workout =>{
+      {/* check if null or not then return  */}
+      {this.props.workouts ? (this.props.workouts.map(workout =>{
         return <div> 
           <h2>{workout.workout_name}</h2>
           <p>{workout.workout_description}</p>
           <img src={workout.image_url} alt="workout" />
         </div> 
-      })}
+      })) : null}
     </div>
     )
   }
