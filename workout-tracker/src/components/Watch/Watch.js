dd468889533173970fc562d745c09df4d81abcfb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 20:49:21 +0200

    created class component Watch

--- workout-tracker/src/components/Watch/Watch.js
+++ workout-tracker/src/components/Watch/Watch.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Watch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Watch;
