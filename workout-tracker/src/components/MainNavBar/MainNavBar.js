65b07cd3dda7085ac41696085ac73ceea2e84455
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 17:49:32 +0200

    bugfix in MainNavBar => deleted parentheses in map method for conventions

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -2,20 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 
-const StyledMainNavBar = styled.nav`
-display: flex;
-justify-content: space-between;
+const StyledMainNavBar = styled.div`
+
 `;
 
 const MainNavBar = () => {
   const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
   return (
     <StyledMainNavBar>
-      {navButtons.map((button, index) => (
+      {navButtons.map((button, index) =>
         <NavLink key={index} to={`/${button}`}>
           {button}
         </NavLink>
-      ))}
+        )}
     </StyledMainNavBar>
   );
 };
