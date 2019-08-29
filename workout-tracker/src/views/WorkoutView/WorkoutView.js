e88233f445c6c797492c3c88b06574d857da6c4e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 20:34:04 +0200

    created startWorkout method

--- workout-tracker/src/views/WorkoutView/WorkoutView.js
+++ workout-tracker/src/views/WorkoutView/WorkoutView.js
@@ -1,31 +1,43 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import styled from 'styled-components';
+import React from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
 
-const StyledWorkoutView = styled.div`
-`;
+const StyledWorkoutView = styled.div``;
 
 class WorkoutView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      time: 0,
-     }
+    this.state = {
+      time: 0
+    };
   }
-  render() { 
-    return ( 
+
+  startWorkout = () => {
+    setInterval(
+      () =>
+        this.setState(prevState => {
+          return {
+            time: prevState.time + 1,
+          };
+        }),
+      1000
+    );
+  };
+
+  render() {
+    return (
       <div>
-      <p>{this.state.time}</p>
-      <button onClick={this.startWorkout}>Start</button>
+        <p>{this.state.time}</p>
+        <button onClick={this.startWorkout}>Start</button>
       </div>
-     );
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-   currentWorkout: state.currentWorkout.currentWorkout,
+    currentWorkout: state.currentWorkout.currentWorkout
   };
 };
- 
+
 export default connect(mapStateToProps)(WorkoutView);
