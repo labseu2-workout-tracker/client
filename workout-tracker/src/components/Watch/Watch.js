0ebdfc232e7eceb88126e906337a7bdf46c9f766
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 20:55:49 +0200

    created StyledWatch variable

--- workout-tracker/src/components/Watch/Watch.js
+++ workout-tracker/src/components/Watch/Watch.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledWatch = styled.div`
+`;
+
 class Watch extends React.Component {
   constructor(props) {
     super(props);
