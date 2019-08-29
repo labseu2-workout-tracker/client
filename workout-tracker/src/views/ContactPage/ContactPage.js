81575acf68c25c1fc076ab5b00152b94a08dbfdd
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 18:40:00 +0200

    added div with className right-side

--- workout-tracker/src/views/ContactPage/ContactPage.js
+++ workout-tracker/src/views/ContactPage/ContactPage.js
@@ -2,20 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledContactPage = styled.div`
+  input {
+    text-align: center;
+  }
 
-input {
-  text-align: center;
-}
+  .row {
+    display: flex;
+    justify-content: space-between;
+  }
 
-.row {
-  display: flex;
-  justify-content: space-between;
-}
-
-.messageInput {
-  height: 8rem;
-  width: 50%;
-}
+  .messageInput {
+    height: 8rem;
+    width: 50%;
+  }
 `;
 
 class ContactPage extends React.Component {
@@ -50,44 +49,44 @@ class ContactPage extends React.Component {
     return (
       <StyledContactPage>
         <h2>Contact Us</h2>
-        <div className="left-side">
-
-        </div>
-        <div className="row">
-          <p>Name:</p>
+        <div className="left-side"></div>
+        <div className="right-side">
+          <div className="row">
+            <p>Name:</p>
+            <input
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              placeholder="John Doe"
+            />
+          </div>
+          <div className="row">
+            <p>Email</p>
+            <input
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="JohnDoe@gmail.com"
+            />
+          </div>
+          <div className="row">
+            <p>Phone:</p>
+            <input
+              name="phone"
+              value={this.state.phone}
+              onChange={this.handleChange}
+              placeholder="(Optional)"
+            />
+          </div>
           <input
-            name="name"
-            value={this.state.name}
+            className="messageInput"
+            name="message"
+            value={this.state.message}
             onChange={this.handleChange}
-            placeholder="John Doe"
+            placeholder="Message"
           />
+          <button onClick={this.sendMessage}>Send</button>
         </div>
-        <div className="row">
-          <p>Email</p>
-          <input
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            placeholder="JohnDoe@gmail.com"
-          />
-        </div>
-        <div className="row">
-          <p>Phone:</p>
-          <input
-            name="phone"
-            value={this.state.phone}
-            onChange={this.handleChange}
-            placeholder="(Optional)"
-          />
-        </div>
-        <input
-        className="messageInput"
-          name="message"
-          value={this.state.message}
-          onChange={this.handleChange}
-          placeholder="Message"
-        />
-        <button onClick={this.sendMessage}>Send</button>
       </StyledContactPage>
     );
   }
