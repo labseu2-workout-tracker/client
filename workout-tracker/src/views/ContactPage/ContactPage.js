e4815aceced2f631325ce12d6f3fd8b67045387a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 18:15:25 +0200

    created one input for tracking message from the user

--- workout-tracker/src/views/ContactPage/ContactPage.js
+++ workout-tracker/src/views/ContactPage/ContactPage.js
@@ -17,6 +17,7 @@ class ContactPage extends React.Component {
         <input name="name" value={this.state.name} onChange={this.handleChange} placeholder="John Doe" />
         <input name="email" value={this.state.email} onChange={this.handleChange} placeholder="JohnDoe@gmail.com" />
         <input name="phone" value={this.state.phone} onChange={this.handleChange} placeholder="(Optional)" />        
+        <input name="message" value={this.state.message} onChange={this.handleChange} placeholder="Message" />                
         </StyledContactPage>
      );
   }
