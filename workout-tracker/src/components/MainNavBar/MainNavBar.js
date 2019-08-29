554dbe1dca6a6cda04be794ad6e65d3e2a761bfe
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 14:29:42 +0200

    loop with map method over navButtons array between nav element which display the different navLinks

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom';
 const MainNavBar = () => {
   const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
   return ( 
+    <nav>
+      {navButtons.map(button => <NavLink to={`/${button}`}>{button}</NavLink>)}
+    </nav>
    );
 }
  
