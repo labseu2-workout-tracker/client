10a646bfac85a9e3c266cc7b1ef1504e4149deb7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 21:25:49 +0200

    comment code out

--- workout-tracker/src/components/Watch/Watch.js
+++ workout-tracker/src/components/Watch/Watch.js
@@ -2,52 +2,52 @@ import React from 'react';
 import Timer from 'react-compound-timer';
 import styled from 'styled-components';
 
-function Stopwatch() {
-  var time = 0;
-  var interval;
-  var offset;
+// function Stopwatch() {
+//   var time = 0;
+//   var interval;
+//   var offset;
 
-  function update() {
-    time += delta();
-    var formattedTime = timeFormatter(time);
-  }
+//   function update() {
+//     time += delta();
+//     var formattedTime = timeFormatter(time);
+//   }
 
-  function delta() {
-    var now = Date.now();
-    var timePassed = now - offset;
-    offset = now;
-    return timePassed;
-  }
+//   function delta() {
+//     var now = Date.now();
+//     var timePassed = now - offset;
+//     offset = now;
+//     return timePassed;
+//   }
 
-  function timeFormatter(timeInMilliSeconds) {
-    var time = new Date(timeInMilliSeconds);
-  }
+//   function timeFormatter(timeInMilliSeconds) {
+//     var time = new Date(timeInMilliSeconds);
+//   }
 
-  this.isOn = false;
+//   this.isOn = false;
 
-  this.start = function() {
-    if(!this.isOn) {
-     interval = setInterval(update, 10);
-     offset = Date.now();
-     this.isOn = true;
-    }
-  };
+//   this.start = function() {
+//     if(!this.isOn) {
+//      interval = setInterval(update, 10);
+//      offset = Date.now();
+//      this.isOn = true;
+//     }
+//   };
   
-  this.stop = function() {
-    if(this.isOn) {
-      clearInterval(interval);
-      intervall = null;
-      this.isOn = false;
-    }
-  };
+//   this.stop = function() {
+//     if(this.isOn) {
+//       clearInterval(interval);
+//       intervall = null;
+//       this.isOn = false;
+//     }
+//   };
   
-  this.reset = function() {
-    time = 0;
-  };
-}
+//   this.reset = function() {
+//     time = 0;
+//   };
+// }
 
-var watch = new Stopwatch();
-watch.start();
+// var watch = new Stopwatch();
+// watch.start();
 
 const StyledWatch = styled.div`
 `;
