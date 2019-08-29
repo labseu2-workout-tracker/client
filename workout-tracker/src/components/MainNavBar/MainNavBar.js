42f362deb2ecea438c5a2be4b1b3e99b450ba857
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-29 17:23:38 +0200

    created StyledMainNavBar variable

--- workout-tracker/src/components/MainNavBar/MainNavBar.js
+++ workout-tracker/src/components/MainNavBar/MainNavBar.js
@@ -2,6 +2,10 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
 
+const StyledMainNavBar = styled.nav`
+
+`;
+
 const MainNavBar = () => {
   const navButtons = ["Home", "Workouts", "Exercises", "Contact"];
   return (
