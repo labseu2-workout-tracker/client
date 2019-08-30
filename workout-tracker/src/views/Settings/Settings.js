a659001f5b0e6425ab7e98fd7a6f6138fc74a389
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 12:17:55 +0200

    created class Settings

--- workout-tracker/src/views/Settings/Settings.js
+++ workout-tracker/src/views/Settings/Settings.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Settings;
