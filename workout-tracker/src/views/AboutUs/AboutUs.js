65e4f8bdc6cb8dbaa8016b0427e16711b7c74b26
Talent Antonio <antonio.talenty@gmail.com>
2019-08-30 10:14:34 +0100

    talent card

--- workout-tracker/src/views/AboutUs/AboutUs.js
+++ workout-tracker/src/views/AboutUs/AboutUs.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import tale from '../../assets/avatar/tale.jpg';
 
 
 const About = () => {
-    return 
-    
+    return <>
+    <section className="container">
+            <div className="container">
+                <h1>Meet The Team</h1>
+                <div className="card-wrapper">
+                    <div className="card">
+                    <img src={tale} alt="card background" className="card-img"/>
+                    <img src={tale} alt="profile" className="profile-img"/>
+                    <h1>Antonio Talent</h1>
+                    </div>
+                </div>
+               </div> 
+            </section>
+    </>
+
   };
   
 export default About;
