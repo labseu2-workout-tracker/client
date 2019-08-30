68d485e4aea39d0c11b3ba8da58d3b8a641734e3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 13:04:06 +0200

    created settings reducer

--- workout-tracker/src/store/reducers/settings.js
+++ workout-tracker/src/store/reducers/settings.js
@@ -3,3 +3,12 @@ import * as type from "../actions/settingActions";
 const initialState = {
   settings: null,  
 };
+
+const settings = (state = initialState, action) => {
+ 
+    default:
+      return state;
+  }
+};
+
+export default workouts;
