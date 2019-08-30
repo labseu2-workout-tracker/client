48fa7d1d42e597f0738e88020dd4a1ec708e3b50
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 12:47:29 +0200

    added variable settings which is the api adress where we fetching from

--- workout-tracker/src/store/actions/settingActions.js
+++ workout-tracker/src/store/actions/settingActions.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 
 export const FETCH_SETTINGS = 'FETCH_SETTINGS';
 export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
+
+const settings = `${process.env.REACT_APP_BASE_URL}/settings`;
+// adress get's changed later
