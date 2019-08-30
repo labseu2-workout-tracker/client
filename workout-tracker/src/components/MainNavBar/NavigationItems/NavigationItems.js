d0a2fa0798673628b4fc1ccfab559d97a71f84ee
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-30 13:23:11 +0400

    ft: create navigation items component to render navigation items based on auth state

--- workout-tracker/src/components/MainNavBar/NavigationItems/NavigationItems.js
+++ workout-tracker/src/components/MainNavBar/NavigationItems/NavigationItems.js
@@ -8,3 +8,23 @@ const navItems = [
   { id: 'login', text: 'Login', link: '/login', auth: false },
   { id: 'signup', text: 'Signup', link: '/signup', auth: false }
 ];
+
+const navigationItems = props => [
+  ...navItems.filter(item => item.auth === props.isAuth).map(item => (
+    <li
+      key={item.id}
+      className={['navigation-item', props.mobile ? 'mobile' : ''].join(' ')}
+    >
+      <NavLink to={item.link} exact onClick={props.onChoose}>
+        {item.text}
+      </NavLink>
+    </li>
+  )),
+  props.isAuth && (
+    <li className="navigation-item" key="logout">
+      <button onClick={props.onLogout}>Logout</button>
+    </li>
+  )
+]
+
+export default navigationItems;
