d9da3fd5c2848916b40c20d5d7fa658de4a9ab22
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-30 12:48:07 +0200

    created actionDispatcher fetchSettings

--- workout-tracker/src/store/actions/settingActions.js
+++ workout-tracker/src/store/actions/settingActions.js
@@ -5,3 +5,14 @@ export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
 
 const settings = `${process.env.REACT_APP_BASE_URL}/settings`;
 // adress get's changed later
+
+export const fetchSettings = () => dispatch => {
+  // type LOADING needs to be added (also for the redux state) 
+  return axios.get(settings)
+    .then(res => {
+      dispatch({ type: FETCH_SETTINGS, exercises: res.data });
+    })
+    .catch(err => {
+   // type ERROR needs to be added (also for the redux state)
+    });
+};
