96d8c1621432e2ece9b2224e9e9f16f5b37abb0a
Talent Antonio <antonio.talenty@gmail.com>
2019-08-30 13:30:20 +0100

    routing the About Us

--- workout-tracker/src/App.js
+++ workout-tracker/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Tabs from './views/Tabs/Tabs';
 import Header from './views/Header/Header';
+// import Workouts from './views/Workouts/Workouts'
 import WorkoutView from './views/WorkoutView/WorkoutView';
 import ContactPage from './views/ContactPage/ContactPage';
 import ExercisesLibrary from './views/ExerciseLibrary/ExercisesLibrary';
 import UserPage from './views/UserPage/UserPage';
+import About from './views/AboutUs/AboutUs'
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 
 import SignupPage from './views/Auth/Signup';
@@ -178,7 +180,8 @@ class App extends Component {
         <Route path={'/Exercises'} component={ExercisesLibrary} />
         <Route path={'/Home'} component={UserPage} />
         <Route path={'/Contact'} component={ContactPage} />    
-        <Route path={'/Workout'} component={WorkoutView} />        
+        <Route path={'/Workout'} component={WorkoutView} /> 
+        <Route path={'/About'} component={About} />           
     </div>
       )
     }
