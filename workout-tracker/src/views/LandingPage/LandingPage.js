51c7e7d9b9c6033c892a986c14dfcf1e652b0f1a
Talent Antonio <antonio.talenty@gmail.com>
2019-08-31 02:00:58 +0100

    background img svg set

--- workout-tracker/src/views/LandingPage/LandingPage.js
+++ workout-tracker/src/views/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './LandingPage.css'
+import bg from '../../assets/landingpage/bg-shape.svg'
 
 const LandingPage = () => {
     return(
        <section>
-
+        <img src={bg} alt="background Shape" id="bg" />
        </section>
     );
 }
