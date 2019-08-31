73b8b0f7fa3cf36e002f972a1911cf48c1c9078a
Talent Antonio <antonio.talenty@gmail.com>
2019-08-31 02:19:56 +0100

    section and styling done

--- workout-tracker/src/views/LandingPage/LandingPage.js
+++ workout-tracker/src/views/LandingPage/LandingPage.js
@@ -6,6 +6,9 @@ const LandingPage = () => {
     return(
        <section>
         <img src={bg} alt="background Shape" id="bg" />
+        <h1>Be<span>Fitr</span><br/>Workout Tracker</h1>
+        <hr />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc omni virtuti vitium contrario nomine opponitur. Hoc est non dividere, sed frangere.</p>
        </section>
     );
 }
