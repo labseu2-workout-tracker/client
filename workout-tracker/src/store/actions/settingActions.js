faefbf00d4db86120925d9678c75b34de4a30714
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-01 21:57:44 +0200

    changed global variable to function variable: userId

--- workout-tracker/src/store/actions/settingActions.js
+++ workout-tracker/src/store/actions/settingActions.js
@@ -5,28 +5,28 @@ export const UPDATE_SETTINGS = "UPDATE_SETTINGS";
 
 const settings = `${process.env.REACT_APP_BASE_URL}/settings`;
 // adress get's changed later
-const userId = localStorage.getItem('userId');
 
 export const fetchSettings = () => dispatch => {
   // type LOADING needs to be added (also for the redux state)
+  const userId = localStorage.getItem('userId');
 
   return axios
-    .get(`${settings}/${userId}`)
-    .then(res => {
-      debugger
+  .get(`${settings}/${userId}`)
+  .then(res => {
       dispatch({ type: FETCH_SETTINGS, settings: res.data });
     })
     .catch(err => {
       // type ERROR needs to be added (also for the redux state)
     });
-};
-
-export const updateSettings = updatedSettings => {
+  };
+  
+  export const updateSettings = updatedSettings => {
+    const userId = localStorage.getItem('userId');
   debugger
   return axios
     .put(`${settings}/${userId}`, updatedSettings)
     .then(res => {
-
+debugger
       return axios.get(settings)
       .then(res => {
         return { type: UPDATE_SETTINGS, updatedSettings: res.data };
@@ -34,5 +34,6 @@ export const updateSettings = updatedSettings => {
     })
     .catch(err => {
       // type ERROR needs to be added (also for the redux state)
+    debugger
     });
 };
--- workout-tracker/src/views/Settings/Settings.js
+++ workout-tracker/src/views/Settings/Settings.js
@@ -65,90 +65,184 @@ class Settings extends React.Component {
         {this.props.settings
           ? this.props.settings.map((setting, index) => {
               return (
-                <div key={index}>
-                  <div className="row">
-                    <p>Email: </p>
-                    <input
+<div
+             key={index}>
+        <div
+            className="user-data"        
+        >
+          <ul className="info">
+            <li>
+              <span className="text">Email:</span>
+              <span className="data">
+              <input
                       value={this.state.email}
                       onChange={this.handleChange}
                       placeholder={setting.email}
                       name="email"
                     />
-                  </div>
-                  <div className="row">
-                    <p>Username: </p>
-                    <input
-                      value={this.state.username}
-                      onChange={this.handleChange}
-                      placeholder={setting.username}
-                      name="username"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Password: </p>
-                    <input
-                      value={this.state.password}
-                      onChange={this.handleChange}
-                      placeholder={setting.password}
-                      name="password"
-                      type="password"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Weight: </p>
-                    <input
-                      value={this.state.weight}
-                      onChange={this.handleChange}
-                      placeholder={setting.weight}
-                      name="weight"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Height: </p>
-                    <input
-                      value={this.state.height}
-                      onChange={this.handleChange}
-                      placeholder={setting.height}
-                      name="height"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Gender: </p>
-                    <input
-                      value={this.state.gender}
-                      onChange={this.handleChange}
-                      placeholder={setting.gender}
-                      name="gender"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>user_level</p>
-                    <input
-                      value={this.state.user_level}
-                      onChange={this.handleChange}
-                      placeholder={setting.user_level}
-                      name="user_level"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Email Notification: </p>
-                    <input
-                      value={this.state.email_notification}
-                      onChange={this.handleChange}
-                      placeholder={setting.email_notification.toString()}
-                      name="email_notification"
-                    />
-                  </div>
-                  <div className="row">
-                    <p>Push Notification: </p>
-                    <input
-                      value={this.state.push_notification}
-                      onChange={this.handleChange}
-                      placeholder={setting.push_notification.toString()}
-                      name="push_notification"
-                    />
-                  </div>
-                </div>
+              </span>
+              <span className="icon">
+              <i className="fa fa-envelope"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Username:</span>
+              <span className="data">
+              <p>{setting.username ? setting.username : "Not specified"}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-user"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Weight:</span>
+              <span className="data">
+              <p>{setting.weight === 0 ? "Not specified" : setting.weight}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-balance-scale"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Height:</span>
+              <span className="data">
+              <p>{setting.height === 0 ? "Not specified" : setting.height}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-arrow-circle-up"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Gender:</span>
+              <span className="data">
+              <p>{setting.gender ? setting.gender : "Not specified"}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-venus-mars"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Level:</span>
+              <span className="data">
+              <p>{setting.user_level ? setting.user_level : "Not specified"}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-graduation-cap"></i>
+              </span>
+            </li>
+            <li>
+              <span className="text">Email Notification:</span>
+              <span className="data">
+              <p>{setting.email_notification.toString()}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-envelope"></i>
+              </span>
+            </li>            
+            <li>
+              <span className="text">Push Notification:</span>
+              <span className="data">
+              <p>{setting.push_notification.toString()}</p>
+              </span>
+              <span className="icon">
+              <i className="fa fa-bell"></i>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <button
+        className="update-button"
+        onClick={this.startUpdate}>Update</button>
+            </div>
+
+
+
+
+
+                // <div key={index}>
+                //   <div className="row">
+                //     <p>Email: </p>
+                //     <input
+                //       value={this.state.email}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.email}
+                //       name="email"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Username: </p>
+                //     <input
+                //       value={this.state.username}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.username}
+                //       name="username"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Password: </p>
+                //     <input
+                //       value={this.state.password}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.password}
+                //       name="password"
+                //       type="password"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Weight: </p>
+                //     <input
+                //       value={this.state.weight}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.weight}
+                //       name="weight"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Height: </p>
+                //     <input
+                //       value={this.state.height}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.height}
+                //       name="height"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Gender: </p>
+                //     <input
+                //       value={this.state.gender}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.gender}
+                //       name="gender"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>user_level</p>
+                //     <input
+                //       value={this.state.user_level}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.user_level}
+                //       name="user_level"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Email Notification: </p>
+                //     <input
+                //       value={this.state.email_notification}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.email_notification.toString()}
+                //       name="email_notification"
+                //     />
+                //   </div>
+                //   <div className="row">
+                //     <p>Push Notification: </p>
+                //     <input
+                //       value={this.state.push_notification}
+                //       onChange={this.handleChange}
+                //       placeholder={setting.push_notification.toString()}
+                //       name="push_notification"
+                //     />
+                //   </div>
+                // </div>
               );
             })
           : null}
