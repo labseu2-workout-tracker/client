12cceddc19b772c0321c21ceb3ffc6d3bbe0323b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-02 23:51:03 +0200

    added comments

--- workout-tracker/src/views/WorkoutView/WorkoutView.js
+++ workout-tracker/src/views/WorkoutView/WorkoutView.js
@@ -51,19 +51,19 @@ class WorkoutView extends React.Component {
             <div>
               <div className="row">
               <p>Set</p>
-              <input type="number" value={this.state.set} onChange={this.handleChange} placeholder={} name="set"/> 
+              <input type="number" value={this.state.set} onChange={this.handleChange}  name="set"/> 
               </div>
               <div className="row">
               <p>Reps</p>
-              <input type="number" value={this.state.reps} onChange={this.handleChange} placeholder={} name="reps"/> 
+              <input type="number" value={this.state.reps} onChange={this.handleChange}  name="reps"/> 
               </div>
               <div className="row">
               <p>Time</p>
-              <input type="number" value={this.state.time} onChange={this.handleChange} placeholder={} name="time"/> 
+              <input type="number" value={this.state.time} onChange={this.handleChange}  name="time"/> 
               </div>
               <div className="row">
               <p>Weight</p>
-              <input type="number" value={this.state.weight} onChange={this.handleChange} placeholder={} name="weight"/> 
+              <input type="number" value={this.state.weight} onChange={this.handleChange}  name="weight"/> 
               </div>
             </div>
           </div>
@@ -109,3 +109,13 @@ export default connect(
   mapStateToProps,
   { fetchWorkoutDetails }
 )(WorkoutView);
+
+
+// description: "Lie back on an incline bench with a dumbbell in each hand atop your thighs. The palms of your hands will be facing each other. Then, using your thighs to help push the dumbbells up, lift the dumbbells one at a time so that you can hold them at shoulder width. Once you have the dumbbells raised to shoulder width, rotate your wrists forward so that the palms of your hands are facing away from you. This will be your starting position. Be sure to keep full control of the dumbbells at all times. Then breathe out and push the dumbbells up with your chest. Lock your arms at the top, hold for a second, and then start slowly lowering the weight. Tip Ideally, lowering the weights should take about twice as long as raising them. Repeat the movement for the prescribed amount of repetitions. When you are done, place the dumbbells back on your thighs and then on the floor. This is the safest manner to release the dumbbells."
+// duration: null
+// equipment: "Dumbbell"
+// exercise_name: "Incline Dumbbell Press"
+// picture_one: "https://www.bodybuilding.com/exercises/exerciseImages/sequences/380/Male/l/380_1.jpg"
+// picture_two: "https://www.bodybuilding.com/exercises/exerciseImages/sequences/380/Male/l/380_2.jpg"
+// reps: 12
+// video: "
