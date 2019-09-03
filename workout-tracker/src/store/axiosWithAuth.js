ee210249d1f4606a274eb34ab31d321f2d528dca
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-03 15:01:43 +0200

    added axiosWithAuth

--- workout-tracker/src/store/axiosWithAuth.js
+++ workout-tracker/src/store/axiosWithAuth.js
@@ -1,2 +1,11 @@
 import axios from 'axios';
 
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+          headers: {
+                  Authorization: token,
+          }
+  });
+};
