7786672f1ed4ee5fea0eb327dc6bf59daa20fd0b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-04 21:08:27 +0200

    rename of redux state

--- workout-tracker/src/store/reducers/workouts.js
+++ workout-tracker/src/store/reducers/workouts.js
@@ -2,8 +2,8 @@ import * as type from "../actions/workoutsActions";
 
 const initialState = {
   workouts: null,
-  workoutDetails: null,
-  currentExercise: null
+  allExercises: null,
+  currentExercise: null,
 };
 
 //  default state
@@ -18,20 +18,28 @@ const workouts = (state = initialState, action) => {
     case type.FETCH_WORKOUT_DETAILS:
       return {
         ...state,
-        workoutDetails: action.workoutDetails
+        allExercises: action.workoutDetails.exercises,
       };
 
     case type.CHOOSE_EXERCISE:
-      const filterCurrentExercise = state.workoutDetails.exercises.filter(
+      const filterCurrentExercise = state.allExercises.exercises.filter(
         exercise => exercise.exercise_name === action.current_exercise
       );
+      debugger
       return {
         ...state,
         currentExercise: filterCurrentExercise
       };
 
       case type.FINISH_EXERCISE:
-          
+          const deleteExercise = state.allExercises.exercises.filter(
+            exercise => exercise.exercise_name === action.current_exercise
+          );
+          debugger
+          return {
+            ...state,
+            currentExercise: deleteExercise
+          };  
 
     default:
       return state;
