c983f759a24e89b3a50e645a4a24465855092907
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-04 09:52:56 +0200

    added var currentNumber

--- workout-tracker/src/store/reducers/exercises.js
+++ workout-tracker/src/store/reducers/exercises.js
@@ -141,6 +141,8 @@ const exercises = (state = initialState, action) => {
           
           // let numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
           
+          let currentNumbers = [];
+          
           
     
             return { ...state, currentButtons: actualButtons };
