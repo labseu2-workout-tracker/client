e0b0fb188d82d4b4624afa1abebbbdc48cf4bbed
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-09-05 13:28:21 +0400

    side drawer close button

--- workout-tracker/src/components/MainNavBar/MobileNavigation/MobileNavigation.js
+++ workout-tracker/src/components/MainNavBar/MobileNavigation/MobileNavigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Button from '../../Button/Button'
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './MobileNavigation.css';
 
 const mobileNavigation = props => (
   <nav className={['mobile-nav', props.open ? 'open' : ''].join(' ')}>
+    <div className='close-button'>
+      <Button design="close" onClick={props.onChooseItem} >X</Button>
+    </div>
     <ul
       className={['mobile-nav__items', props.mobile ? 'mobile' : ''].join(' ')}
     >
