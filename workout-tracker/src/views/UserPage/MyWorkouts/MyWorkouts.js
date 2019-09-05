9491e22b17aceb27455ea6a10988fac6b185a867
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-05 11:24:32 +0200

    added map method which displays saved workouts

--- workout-tracker/src/views/UserPage/MyWorkouts/MyWorkouts.js
+++ workout-tracker/src/views/UserPage/MyWorkouts/MyWorkouts.js
@@ -14,6 +14,19 @@ class WorkoutView extends React.Component {
           <h1>
             My Workouts
             </h1>
+
+            <div className='land-wrapper'>
+      {this.props.myWorkouts ? (this.props.myWorkouts.map((workout, index) =>{
+        return <div key={index} className='workout-card'> 
+          <img src={workout.image_url} alt='workout' className='workout-img'/>
+          <h1>{workout.workout_name}</h1>
+          <p>{workout.workout_description}</p>
+          <Link onClick={this.props.fetchWorkoutDetails(workout.id)} to='Workout_session' className='btn'>
+                Start Workout
+              </Link>
+        </div> 
+      })) : null}
+    </div>
             </div>;
   }
 }
