e37460b4027cd35fc21696c964698e9ee31ad64d
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-05 19:50:25 +0100

    refactor App.js

--- workout-tracker/src/App.js
+++ workout-tracker/src/App.js
@@ -1,24 +1,24 @@
-import React, { Component, Fragment } from 'react';
-import Settings from './views/Settings/Settings';
-import ContactPage from './views/ContactPage/ContactPage';
-import ExercisesLibrary from './views/ExerciseLibrary/ExercisesLibrary';
-import UserPage from './views/UserPage/UserPage';
-import About from './views/AboutUs/AboutUs'
-import LandingPage from './views/LandingPage/LandingPage';
-import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
-import WorkoutSession from './views/WorkoutSession/WorkoutSession';
+import React, { Component, Fragment } from "react";
+import Settings from "./views/Settings/Settings";
+import ContactPage from "./views/ContactPage/ContactPage";
+import ExercisesLibrary from "./views/ExerciseLibrary/ExercisesLibrary";
+import UserPage from "./views/UserPage/UserPage";
+import About from "./views/AboutUs/AboutUs";
+import LandingPage from "./views/LandingPage/LandingPage";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import WorkoutSession from "./views/WorkoutSession/WorkoutSession";
 import PrivateRoute from "./auth/PrivateRoute";
-import Auth from './auth/auth';
-import SignupPage from './views/Auth/Signup';
-import LoginPage from './views/Auth/Login';
-import MainNavBar from './components/MainNavBar/MainNavBar';
-import MobileNavigation from './components/MainNavBar/MobileNavigation/MobileNavigation'
-import Toolbar from './components/Toolbar/Toolbar';
-import Layout from './components/Layout/Layout';
-import Backdrop from './components/Backdrop/Backdrop';
+import Auth from "./auth/auth";
+import SignupPage from "./views/Auth/Signup";
+import LoginPage from "./views/Auth/Login";
+import MainNavBar from "./components/MainNavBar/MainNavBar";
+import MobileNavigation from "./components/MainNavBar/MobileNavigation/MobileNavigation";
+import Toolbar from "./components/Toolbar/Toolbar";
+import Layout from "./components/Layout/Layout";
+import Backdrop from "./components/Backdrop/Backdrop";
 
-import './App.css';
-import Workouts from './views/Workouts/Workouts';
+import "./App.css";
+import Workouts from "./views/Workouts/Workouts";
 
 class App extends Component {
   state = {
@@ -41,35 +41,23 @@ class App extends Component {
 
   render() {
     let routes = (
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/about" component={About} />
-        <Route exact path="/contact" component={ContactPage} />
-        <Route
-          path="/login"
-          render={props => (
-            <LoginPage
-              {...props}
-            />
-          )}
-        />
-        <Route
-          path="/signup"
-          render={props => (
-            <SignupPage
-              {...props}
-            />
-          )}
-        />
-        <PrivateRoute path={"/Dashboard"} component={UserPage} />
-        <PrivateRoute path={"/Exercises"} component={ExercisesLibrary} />
-        <PrivateRoute path={"/Settings"} component={Settings} />
-        <PrivateRoute path={"/Contact"} component={ContactPage} />
-        <PrivateRoute path={"/Workouts"} component={Workouts} />
-        <PrivateRoute path={'/Workout_session'} component={WorkoutSession} />  
-        <PrivateRoute path={"/About"} component={About} />
-        <Redirect to="/" />
-      </Switch>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/about" component={About} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route path="/login" render={props => <LoginPage {...props} />} />
+          <Route path="/signup" render={props => <SignupPage {...props} />} />
+          <PrivateRoute path={"/Dashboard"} component={UserPage} />
+          <PrivateRoute path={"/Exercises"} component={ExercisesLibrary} />
+          <PrivateRoute path={"/Settings"} component={Settings} />
+          <PrivateRoute path={"/Contact"} component={ContactPage} />
+          <PrivateRoute path={"/Workouts"} component={Workouts} />
+          <PrivateRoute path={"/Workout_session"} component={WorkoutSession} />
+          <PrivateRoute path={"/About"} component={About} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
     );
     return (
       <Fragment>
