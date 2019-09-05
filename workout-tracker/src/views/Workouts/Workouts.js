d38b2e04cf17fa13f1cc7709dd7e8f2fadceaacf
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-05 10:40:57 +0200

    added add Workout Link with onClick method

--- workout-tracker/src/views/Workouts/Workouts.js
+++ workout-tracker/src/views/Workouts/Workouts.js
@@ -27,6 +27,9 @@ class Workouts  extends React.Component {
           <Link onClick={this.props.fetchWorkoutDetails(workout.id)} to='Workout_session' className='btn'>
                 Start Workout
               </Link>
+              <Link onClick={this.props.addWorkout(workout.id)} to='/dashboard/myworkouts' className='btn'>
+                Add Workout
+              </Link>
         </div> 
       })) : null}
     </div>
