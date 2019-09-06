4e7788c5bbe0aa6fe1577fd9aee23826177dac63
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-06 19:07:48 +0200

    added Header

--- workout-tracker/src/components/Exercises/AllExercises.js
+++ workout-tracker/src/components/Exercises/AllExercises.js
@@ -125,6 +125,17 @@ const AllExercises = props => {
     //     </div>
 
     <Layout>
+      <Header className="header">
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        
+      </Menu>
+    </Header>
       <Layout>
         <Sider width={200} style={{ background: "#fff" }}>
           <Menu
