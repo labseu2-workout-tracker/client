25b1cafcb26b1da509dad16ed3a4bb420776205b
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-06 15:15:36 +0100

    remove logger

--- workout-tracker/src/index.js
+++ workout-tracker/src/index.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
-import thunk from 'redux-thunk';
-import rootReducer from './store/reducers';
-import logger from "redux-logger";
-import App from './App';
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers";
+import App from "./App";
 
-import './index.css';
+import "./index.css";
 
 function saveToLocalStorage(state) {
   try {
-          const serializedState = JSON.stringify(state);
-          localStorage.setItem('state', serializedState);
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
   } catch (e) {
-          console.log(e)
+    console.log(e);
   }
 }
 
 function loadFromLocalStorage() {
   try {
-          const serializedState = localStorage.getItem('state');
-          if (serializedState === null) return undefined
-          return JSON.parse(serializedState)
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
   } catch (e) {
-          console.log(e)
-          return undefined
+    console.log(e);
+    return undefined;
   }
 }
 
 const persistedState = loadFromLocalStorage();
 
-const store = createStore(rootReducer,persistedState, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
 
 store.subscribe(() => saveToLocalStorage(store.getState()));
 
@@ -43,4 +42,5 @@ ReactDOM.render(
       <App />
     </Router>
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
