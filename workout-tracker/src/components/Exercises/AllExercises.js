7062521e095439a8e00348ea73bcaa9ce3106956
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-06 18:45:20 +0200

    added layout

--- workout-tracker/src/components/Exercises/AllExercises.js
+++ workout-tracker/src/components/Exercises/AllExercises.js
@@ -134,19 +134,6 @@ const AllExercises = props => {
 //     </div>
 
 <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
     <Layout>
       <Sider width={200} style={{ background: '#fff' }}>
         <Menu
@@ -160,7 +147,7 @@ const AllExercises = props => {
             title={
               <span>
                 <Icon type="user" />
-                subnav 1
+                Muscles
               </span>
             }
           >
