7f72bbd7c1677d29d464c3dd5aa4e18f995a405e
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-09-06 09:28:21 +0100

    redirect to worklout page

--- workout-tracker/src/store/actions/authenticationActions.js
+++ workout-tracker/src/store/actions/authenticationActions.js
@@ -25,11 +25,11 @@ export const doLogIn = (user, history) => dispatch => {
       dispatch(genericAction(LOGIN, userId));
       localStorage.setItem("token", token);
       localStorage.setItem("userId", userId);
-      history.push("/Dashboard");
+      history.push("/workouts");
     })
     .catch(error => {
       let { errorMessage } = error.response.data;
-        dispatch(genericAction(LOGIN_ERROR, errorMessage))
+      dispatch(genericAction(LOGIN_ERROR, errorMessage));
     })
     .finally(() => dispatch(genericAction(LOADING_USER, false)));
 };
@@ -43,12 +43,12 @@ export const doSignUp = (user, history) => dispatch => {
       dispatch(genericAction(LOGIN, userId));
       localStorage.setItem("token", token);
       localStorage.setItem("userId", userId);
-      history.push("/Dashboard");
+      history.push("/workouts");
     })
     .catch(error => {
       let { errorMessage } = error.response.data;
-        dispatch(genericAction(SIGNUP_ERROR, errorMessage))
-    }) 
+      dispatch(genericAction(SIGNUP_ERROR, errorMessage));
+    })
     .finally(() => dispatch(genericAction(LOADING_USER, false)));
 };
 
@@ -64,6 +64,8 @@ export const doGetUser = () => dispatch => {
     .then(response => {
       dispatch(genericAction(GET_USER, response.data));
     })
-    .catch(error => dispatch(genericAction(USER_ERROR, error.response.data.errorMessage)))
+    .catch(error =>
+      dispatch(genericAction(USER_ERROR, error.response.data.errorMessage))
+    )
     .finally(() => dispatch(genericAction(LOADING_USER, false)));
 };
