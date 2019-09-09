2a524d777d68bd5efa3a67db8682fdd83c87ed40
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-09 23:12:13 +0200

    created class MonthlyChart

--- workout-tracker/src/views/UserPage/Tracker/MonthlyChart.js
+++ workout-tracker/src/views/UserPage/Tracker/MonthlyChart.js
@@ -1,2 +1,49 @@
 import React from "react";
 import ReactApexChart from "react-apexcharts";
+
+class MonthlyChart extends React.Component {
+      
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      options: {
+        theme: {
+          monochrome: {
+            enabled: true
+          }
+        },
+        title: {
+          text: "Number of leads"
+        },
+        responsive: [{
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: 'bottom'
+            }
+          }
+        }]
+      },
+      series: [25, 15, 44, 55, 41, 17],
+    }
+  }
+
+ 
+
+  render() {
+    return (
+      
+
+      <div id="chart">
+        <ReactApexChart options={{...this.state.options, labels: this.state.labels}} series={this.state.series} type="pie" width="500px" />
+      </div>
+    )
+  }
+}
+
+export default MonthlyChart;
