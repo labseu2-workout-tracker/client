ea4b707ead56a1d161e7b5f3130559e92ff9e6ba
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-09 21:05:25 +0200

    delete padding

--- workout-tracker/src/components/Exercises/AllExercises.js
+++ workout-tracker/src/components/Exercises/AllExercises.js
@@ -165,7 +165,7 @@ class AllExercises extends React.Component {
 
             <Layout
               style={{
-                // padding: "0 24px 24px",
+              
                 backgroundColor: "#FFF"
               }}
             >
