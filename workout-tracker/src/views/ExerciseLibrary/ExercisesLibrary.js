2f45144a98e149a9fdd973a1e3ddfb7ab982fd7f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-09 18:36:41 +0200

    delte showEquipment

--- workout-tracker/src/views/ExerciseLibrary/ExercisesLibrary.js
+++ workout-tracker/src/views/ExerciseLibrary/ExercisesLibrary.js
@@ -6,17 +6,6 @@ import { connect } from "react-redux";
 
 class ExerciseLibrary extends React.Component {
 
-
-  showEquipment = equipment => {
-    this.props.showEquipment(equipment);
-
-    const muscleButton = document.querySelector(".muscles");
-    const equipmentButton = document.querySelector(".equipment");
-    equipmentButton.click();
-
-    setTimeout(() => muscleButton.click(), 1000);
-  };
-
   render() {
     if (this.props.singleExercise) {
       return (
