0380bec9072a5c71d3fb8372217cbe2045d79362
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-10 19:48:10 +0200

    add pagination

--- workout-tracker/src/views/UserPage/UserHistory/UserHistory.js
+++ workout-tracker/src/views/UserPage/UserHistory/UserHistory.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { fetchWorkoutsHistory } from "../../../store/actions/historyActions";
 import { fetchWorkouts } from "../../../store/actions/workoutsActions";
 import { connect } from "react-redux";
-import { Pagination } from 'antd';
+import { Pagination } from "antd";
 
 class SessionHistory extends React.Component {
   constructor(props) {
@@ -13,7 +13,7 @@ class SessionHistory extends React.Component {
       maxValue: 9
     };
   }
- 
+
   componentDidMount() {
     this.props.fetchWorkoutsHistory();
 
@@ -42,66 +42,76 @@ class SessionHistory extends React.Component {
       <div>
         <h2>Here you can check out the work you have done!</h2>
         <List>
-          {history[0] ? (<div>
-            {history.slice(this.state.minValue, this.state.maxValue).map(session => {
-              const date1 = session.session_start;
-              const date2 = session.session_end;
+          {history[0] ? (
+            <div>
+              {history
+                .slice(this.state.minValue, this.state.maxValue)
+                .map(session => {
+                  const date1 = session.session_start;
+                  const date2 = session.session_end;
 
-              // Extract starting point
-              const startingPoint =
-                date1 === null ? "00:00:00" : date1.slice(11, 17);
-              const endPoint =
-                date2 === null ? "00:00:00" : date2.slice(11, 17);
+                  // Extract starting point
+                  const startingPoint =
+                    date1 === null ? "00:00:00" : date1.slice(11, 17);
+                  const endPoint =
+                    date2 === null ? "00:00:00" : date2.slice(11, 17);
 
-              function pluralize(hours) {
-                return hours <= 1 ? "hour" : "hours";
-              }
+                  function pluralize(hours) {
+                    return hours <= 1 ? "hour" : "hours";
+                  }
 
-              function diff(start, end) {
-                start = start.split(":");
-                end = end.split(":");
-                var startDate = new Date(0, 0, 0, start[0], start[1], 0);
-                var endDate = new Date(0, 0, 0, end[0], end[1], 0);
-                var diff = endDate.getTime() - startDate.getTime();
-                var hours = Math.floor(diff / 1000 / 60 / 60);
-                diff -= hours * 1000 * 60 * 60;
-                var minutes = Math.floor(diff / 1000 / 60);
+                  function diff(start, end) {
+                    start = start.split(":");
+                    end = end.split(":");
+                    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+                    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+                    var diff = endDate.getTime() - startDate.getTime();
+                    var hours = Math.floor(diff / 1000 / 60 / 60);
+                    diff -= hours * 1000 * 60 * 60;
+                    var minutes = Math.floor(diff / 1000 / 60);
 
-                // If using time pickers with 24 hours format, add the below line get exact hours
-                if (hours <= 0) {
-                  return `${minutes} minutes`;
-                }
-                return `${hours} ${pluralize(hours)} ${minutes} minutes`;
-              }
+                    // If using time pickers with 24 hours format, add the below line get exact hours
+                    if (hours <= 0) {
+                      return `${minutes} minutes`;
+                    }
+                    return `${hours} ${pluralize(hours)} ${minutes} minutes`;
+                  }
 
-              return (
-                <ol key={session.id}>
-                  <li>
-                    <p>
-                      <strong>Session Started : </strong>
-                      {session.session_start.slice(0, 10)}
-                    </p>
-                    <p>
-                      <strong>Workout Name : </strong>
-                      {workouts === undefined ? (
-                        <h2>Loadin workouts...</h2>
-                      ) : (
-                        workouts.map(item => {
-                          if (session.workout_id === item.id) {
-                            return item.workout_name;
-                          }
-                          return null;
-                        })
-                      )}
-                    </p>
-                    <p>
-                      <strong>Duration : </strong>
-                      {diff(startingPoint, endPoint)}
-                    </p>
-                  </li>
-                </ol>
-              );
-            })}</div>
+                  return (
+                    <ol key={session.id}>
+                      <li>
+                        <p>
+                          <strong>Session Started : </strong>
+                          {session.session_start.slice(0, 10)}
+                        </p>
+                        <p>
+                          <strong>Workout Name : </strong>
+                          {workouts === undefined ? (
+                            <h2>Loadin workouts...</h2>
+                          ) : (
+                            workouts.map(item => {
+                              if (session.workout_id === item.id) {
+                                return item.workout_name;
+                              }
+                              return null;
+                            })
+                          )}
+                        </p>
+                        <p>
+                          <strong>Duration : </strong>
+                          {diff(startingPoint, endPoint)}
+                        </p>
+                      </li>
+                    </ol>
+                  );
+                })}
+              <Pagination
+                defaultCurrent={1}
+                defaultPageSize={5}
+                onChange={this.handleChange}
+                total={this.props.history.length}
+              />
+            </div>
           ) : (
             <p>You have no workout history at the moment</p>
           )}
