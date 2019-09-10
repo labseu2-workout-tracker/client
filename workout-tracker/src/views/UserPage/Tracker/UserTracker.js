accf1a445bce05d9a77f54a114436ee9645750ac
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-10 13:10:40 +0200

    added ternary operator to new divs and added styling

--- workout-tracker/src/views/UserPage/Tracker/UserTracker.js
+++ workout-tracker/src/views/UserPage/Tracker/UserTracker.js
@@ -15,39 +15,61 @@ const StyledUserTracker = styled.div`
   margin-top: 20px;
   height: 400px;
   /* border: 1px solid black; */
+
+  .off {
+    display: none;
+  }
+
+  .weekly , .monthly, .yearly {
+  width: 100%;
+  }
 `;
 
-class UserTracker extends Component {
+class UserTracker extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       weekly: true,
       monthly: false,
-      yearly: false,
-    }
+      yearly: false
+    };
   }
-  handleChange = (value) => {
-            console.log(`selected ${value}`);
-          }
+  handleChange = value => {
+    this.setState({
+      weekly: false,
+      monthly: false,
+      yearly: false
+    });
+    
+    this.setState({
+      [value]: true,
+    });
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <StyledUserTracker>
-      <Select
-        defaultValue="weekly"
-        style={{ width: 120 }}
-        onChange={handleChange}
+        <Select
+          defaultValue="weekly"
+          style={{ width: 120 }}
+          onChange={this.handleChange}
         >
-        <Option value="weekly">Weekly</Option>
-        <Option value="monthly">Monthly</Option>
-        <Option value="yearly">Yearly</Option>
-      </Select>
-      <WeeklyChart />
-      <MonthlyChart />
-      <YearlyChart />
-    </StyledUserTracker>
-     );
+          <Option value="weekly">Weekly</Option>
+          <Option value="monthly">Monthly</Option>
+          <Option value="yearly">Yearly</Option>
+        </Select>
+        <div className={this.state.weekly ? "weekly" : "off"}>
+        <WeeklyChart />
+        </div>
+        <div className={this.state.monthly ? "monthly" : "off"}>
+        <MonthlyChart />
+        </div>
+        <div className={this.state.yearly ? "yearly" : "off"}>
+        <YearlyChart />
+        </div>
+      </StyledUserTracker>
+    );
   }
 }
- 
+
 export default UserTracker;
