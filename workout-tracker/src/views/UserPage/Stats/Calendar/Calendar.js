5ba2caaed93870f651a57b47e098bab4e7dcff4e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-10 21:45:39 +0200

    created class calendar

--- workout-tracker/src/views/UserPage/Stats/Calendar/Calendar.js
+++ workout-tracker/src/views/UserPage/Stats/Calendar/Calendar.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { Calendar } from 'antd';
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Calendar;
