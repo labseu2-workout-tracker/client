759b61721317c2f1c86b4ae5c130778952ea3b9c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-10 21:46:46 +0200

    added calendar to component

--- workout-tracker/src/views/UserPage/Stats/Calendar/Calendar.js
+++ workout-tracker/src/views/UserPage/Stats/Calendar/Calendar.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Calendar } from 'antd';
+import React from "react";
+import { Calendar } from "antd";
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
-  render() { 
-    return (  );
+
+  onPanelChange = (value, mode) => {
+    console.log(value, mode);
+  };
+
+  render() {
+    return <Calendar onPanelChange={onPanelChange} />;
   }
 }
- 
+
 export default Calendar;
