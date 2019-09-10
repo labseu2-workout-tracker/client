c883ac09ac25882915d3d7d34acb0f0d58b58646
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-10 20:51:49 +0200

    created class Stats

--- workout-tracker/src/views/UserPage/Stats/Stats.js
+++ workout-tracker/src/views/UserPage/Stats/Stats.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Stats extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Stats;
