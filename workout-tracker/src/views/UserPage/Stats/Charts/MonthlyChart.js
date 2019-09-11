36a6ced956e3647ebf2cb503ff6036810d21c453
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-11 20:58:57 +0200

    bugfix for setting the dataset for the monthly chart

--- workout-tracker/src/views/UserPage/Stats/Charts/MonthlyChart.js
+++ workout-tracker/src/views/UserPage/Stats/Charts/MonthlyChart.js
@@ -8,7 +8,7 @@ class MonthlyChart extends React.Component {
 
     this.state = {
       labels: ["Red", "Green", "Yellow"],
-      data: [],
+      data: [1, 5, 6, 7],
       backgroundColor: [
         "#f6f078",
         "#01d28e",
@@ -33,122 +33,120 @@ class MonthlyChart extends React.Component {
   }
 
 
-  componentDidMount = () => {
-    let workoutNames = [];
-    let workouts = [];
-    axiosWithAuth()
-      .get(`${process.env.REACT_APP_BASE_URL}/workouts`)
-      .then(res => {
-        res.data.map(workout => {
-          workoutNames.push(workout.workout_name);
-          workouts.push(workout);
-          return workout;
-        });
+  // componentDidMount = () => {
+  //   let workoutNames = [];
+  //   let workouts = [];
+  //   axiosWithAuth()
+  //     .get(`${process.env.REACT_APP_BASE_URL}/workouts`)
+  //     .then(res => {
+  //       res.data.map(workout => {
+  //         workoutNames.push(workout.workout_name);
+  //         workouts.push(workout);
+  //         return workout;
+  //       });
 
-        axiosWithAuth()
-          .get(`${process.env.REACT_APP_BASE_URL}/workouts/history`)
-          .then(res => {
-            let date = new Date();
-            let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-            let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  //       axiosWithAuth()
+  //         .get(`${process.env.REACT_APP_BASE_URL}/workouts/history`)
+  //         .then(res => {
+  //           let date = new Date();
+  //           let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  //           let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-            var getDaysArray = function(s, e) {
-              for (var a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
-                a.push(new Date(d));
-              }
-              return a;
-            };
+  //           var getDaysArray = function(s, e) {
+  //             for (var a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
+  //               a.push(new Date(d));
+  //             }
+  //             return a;
+  //           };
 
-            let daylist = getDaysArray(firstDay, lastDay);
-            daylist.map(v => v.toISOString().slice(0, 10)).join("");
+  //           let daylist = getDaysArray(firstDay, lastDay);
+  //           daylist.map(v => v.toISOString().slice(0, 10)).join("");
 
-            let daysInMonth = [];
+  //           let daysInMonth = [];
 
-            function formatDate(date) {
-              var d = new Date(date),
-                month = "" + (d.getMonth() + 1),
-                day = "" + d.getDate(),
-                year = d.getFullYear();
+  //           function formatDate(date) {
+  //             var d = new Date(date),
+  //               month = "" + (d.getMonth() + 1),
+  //               day = "" + d.getDate(),
+  //               year = d.getFullYear();
 
-              if (month.length < 2) month = "0" + month;
-              if (day.length < 2) day = "0" + day;
+  //             if (month.length < 2) month = "0" + month;
+  //             if (day.length < 2) day = "0" + day;
 
-              return [year, month, day].join("-");
-            }
+  //             return [year, month, day].join("-");
+  //           }
 
-            for (let i = 0; i < daylist.length; i++) {
-              daysInMonth.push(
-                formatDate(daylist[i])
-                  .split("-")
-                  .join("")
-              );
-            }
+  //           for (let i = 0; i < daylist.length; i++) {
+  //             daysInMonth.push(
+  //               formatDate(daylist[i])
+  //                 .split("-")
+  //                 .join("")
+  //             );
+  //           }
 
-            let userHistory = [...res.data.workoutHistory];
-            let resultOfWeek = [];
+  //           let userHistory = [...res.data.workoutHistory];
+  //           let resultOfWeek = [];
 
-            for (let j = 0; j < daysInMonth.length; j++) {
-              for (let i = 0; i < userHistory.length; i++) {
-                if (
-                  userHistory[i].session_start
-                    .match(/.{1,10}/g)[0]
-                    .split("-")
-                    .join("") === daysInMonth[j]
-                ) {
-                  resultOfWeek.push(userHistory[i]);
-                }
-              }
-            }
+  //           for (let j = 0; j < daysInMonth.length; j++) {
+  //             for (let i = 0; i < userHistory.length; i++) {
+  //               if (
+  //                 userHistory[i].session_start
+  //                   .match(/.{1,10}/g)[0]
+  //                   .split("-")
+  //                   .join("") === daysInMonth[j]
+  //               ) {
+  //                 resultOfWeek.push(userHistory[i]);
+  //               }
+  //             }
+  //           }
 
-            let hashTable = {};
+  //           let hashTable = {};
 
-            for (let j = 0; j < workouts.length; j++) {
-              hashTable[workouts[j].workout_name] = 0;
-            }
+  //           for (let j = 0; j < workouts.length; j++) {
+  //             hashTable[workouts[j].workout_name] = 0;
+  //           }
 
-            for (let i = 0; i < resultOfWeek.length; i++) {
-              for (let j = 0; j < workouts.length; j++) {
-                if (resultOfWeek[i].workout_id === workouts[j].id) {
-                  if (hashTable[workouts[j].workout_name]) {
-                    hashTable[workouts[j].workout_name] += 1;
-                  } else {
-                    hashTable[workouts[j].workout_name] = 1;
-                  }
-                }
-              }
-            }
+  //           for (let i = 0; i < resultOfWeek.length; i++) {
+  //             for (let j = 0; j < workouts.length; j++) {
+  //               if (resultOfWeek[i].workout_id === workouts[j].id) {
+  //                 if (hashTable[workouts[j].workout_name]) {
+  //                   hashTable[workouts[j].workout_name] += 1;
+  //                 } else {
+  //                   hashTable[workouts[j].workout_name] = 1;
+  //                 }
+  //               }
+  //             }
+  //           }
 
-            let valuesForDataset = [];
+  //           let valuesForDataset = [];
 
-            let copyOfOptions = this.state.options;
+  //           // let copyOfOptions = this.state.options;
 
-            for (var value in hashTable) {
-              valuesForDataset.push(hashTable[value]);
-            }
-            copyOfOptions.labels = workoutNames;
+  //           for (var value in hashTable) {
+  //             valuesForDataset.push(hashTable[value]);
+  //           }
+  //           // copyOfOptions.labels = workoutNames;
 
-            this.setState({
-              data: valuesForDataset,
-              labels: workoutNames
-            });
-          });
-      });
-  };
+  //           this.setState({
+  //             data: valuesForDataset,
+  //             labels: workoutNames
+  //           });
+  //         });
+  //     });
+  // };
 
   render() {
     return (
       <div style={{ position: "relative", width: "100%", height: "100%" }}>
         <h2>Monthly Results</h2>
         <Polar
-          data={{
+          data={{datasets: [{
+            data: this.state.data,
+            backgroundColor: this.state.backgroundColor,
+            hoverBackgroundColor: this.state.hoverBackgroundColor,
+            label: "Monthly Results",
+          }],
             labels: this.state.labels,
-            datasets: [
-              {
-                data: this.state.data,
-                backgroundColor: this.state.backgroundColor,
-                hoverBackgroundColor: this.state.hoverBackgroundColor
-              }
-            ]
           }}
         />
       </div>
