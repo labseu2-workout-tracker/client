2996cd55e3a3715539fea6d41ac000714844b240
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-11 23:47:13 +0200

    import message and button

--- workout-tracker/src/views/Workouts/Workouts.js
+++ workout-tracker/src/views/Workouts/Workouts.js
@@ -8,6 +8,7 @@ import {
 } from "../../store/actions/workoutsActions";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { message, Button } from 'antd';
 
 const StyledWorkouts = styled.div`
 text-align: center;
