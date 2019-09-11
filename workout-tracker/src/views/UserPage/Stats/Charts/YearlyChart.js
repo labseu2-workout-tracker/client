aa717396679150868eb0f446191f80a778cda743
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-11 10:44:48 +0200

    changed vars to let

--- workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
+++ workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
@@ -56,13 +56,13 @@ class YearlyChart extends React.Component {
             let first_day_year = new Date(year, 0, 1);
             let last_day_year = new Date(year, 11, 31);
 
-            var getDaysArray = function(s, e) {
-              for (var a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
+            let getDaysArray = function(s, e) {
+              for (let a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
                 a.push(new Date(d));
               }
               return a;
             };
-            var daylist = getDaysArray(
+            let daylist = getDaysArray(
               first_day_year,
               last_day_year
             );
@@ -71,7 +71,7 @@ class YearlyChart extends React.Component {
             let daysInYear = [];
 
             function formatDate(date) {
-              var d = new Date(date),
+              let d = new Date(date),
                   month = '' + (d.getMonth() + 1),
                   day = '' + d.getDate(),
                   year = d.getFullYear();
