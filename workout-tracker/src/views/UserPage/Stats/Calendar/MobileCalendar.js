9d422f0292967c133aec9aadfd362240c299c4be
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-11 15:46:48 +0200

    added filter method

--- workout-tracker/src/views/UserPage/Stats/Calendar/MobileCalendar.js
+++ workout-tracker/src/views/UserPage/Stats/Calendar/MobileCalendar.js
@@ -3,27 +3,28 @@ import { Calendar } from "antd";
 import { axiosWithAuth } from "../../../../store/axiosWithAuth";
 import styled from "styled-components";
 
-const StyledMobileCalendar = styled.div`
-`;
+const StyledMobileCalendar = styled.div``;
 
 class MobileCalendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: null,
+      history: null
     };
   }
 
   componentDidMount = () => {
     axiosWithAuth()
-    .get(`${process.env.REACT_APP_BASE_URL}/workouts/history`)
-    .then(res => this.setState({
-      history: res.data.workoutHistory,
-    }));
+      .get(`${process.env.REACT_APP_BASE_URL}/workouts/history`)
+      .then(res =>
+        this.setState({
+          history: res.data.workoutHistory
+        })
+      );
   };
 
   showDate = value => {
-    function formatDate(date) {
+  const formatDate = (date) => {
       var d = new Date(date),
         month = "" + (d.getMonth() + 1),
         day = "" + d.getDate(),
@@ -33,8 +34,15 @@ class MobileCalendar extends React.Component {
       if (day.length < 2) day = "0" + day;
 
       return [year, month, day].join("-");
-    }
-    console.log(formatDate(value._d), this.state.history);
+    };
+
+    const filterWorkoutsForDate = this.state.history.filter(
+      workout =>
+      workout.session_start.match(/.{1,10}/g)[0] === formatDate(value._d)
+      );
+
+
+      console.log(value);
   };
 
   render() {
