66cd71e94c3ef73775f86244597f42ebafa1e8c4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-11 18:07:52 +0200

    changed icon

--- workout-tracker/src/components/Exercises/AllExercises.js
+++ workout-tracker/src/components/Exercises/AllExercises.js
@@ -186,9 +186,9 @@ class AllExercises extends React.Component {
                 }
               >
                 <div className="ant-dropdown-link">
-                  <i className="fa fa-arrow-down down down-hidden" />
+                  <i className="fa fa-chevron-down down down-hidden" />
                   <Button type="primary">Muscles</Button>
-                  <i className="fa fa-arrow-down down" />
+                  <i className="fa fa-chevron-down down" />
                 </div>
               </Dropdown>
               <Dropdown
@@ -205,9 +205,9 @@ class AllExercises extends React.Component {
                 }
               >
                 <div className="ant-dropdown-link">
-                  <i className="fa fa-arrow-down down down" />
+                  <i className="fa fa-chevron-down down down" />
                   <Button type="primary">Equipment</Button>
-                  <i className="fa fa-arrow-down down down-hidden" />
+                  <i className="fa fa-chevron-down down down-hidden" />
                 </div>
               </Dropdown>
             </div>
