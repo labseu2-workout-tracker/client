78cd6282a9d48644d93fa18ce1164dbefd93feeb
Talent Antonio <antonio.talenty@gmail.com>
2019-09-11 12:08:43 +0100

    fix the new excer

--- workout-tracker/src/components/Exercises/SingleExercise.js
+++ workout-tracker/src/components/Exercises/SingleExercise.js
@@ -1,37 +1,49 @@
-import React from "react";
+import React from 'react';
 // import ExerciseRating from "./ExerciseRating";
 import './SingleExcercise.css';
+import { Layout } from 'antd';
 
-const SingleExercise = props => {
+const { Content } = Layout;
+
+const SingleExercise = (props) => {
   return (
     <>
-<div className="exer-1">
-  <div className="close-exercise" onClick={props.closeExercise}>
-    <i className="fa fa-window-close x" />
-  </div>
-  <div className="">
-    <video className="exer-img" controls>
-      <source src={props.exercise.video} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-    <h1>{props.exercise.exercise_name}</h1>
+      <Content style={{ margin: '24px 16px 0' }}> Content
+        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+          <div className="">
+            <div className="">
+              <div onClick={props.closeExercise}>
+                <i className="fa fa-window-close x" />
+              </div>
+              <video className="" controls>
+                <source src={props.exercise.video} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+              <h1>{props.exercise.exercise_name}</h1>
 
-   <div className="description">
-      <i className="fas fa-running"></i> {props.exercise.description}
-    </div>
-   <div className="para">
-    <p> Level: {props.exercise.difficulty}</p>
-    <p> Type: {props.exercise.type}</p>
-    <p>Target: {props.exercise.muscle}</p>
-    <p> Equipment: {props.exercise.equipment}</p>
-   </div>
-   <div className="last-img">
-      <img  src={props.exercise.picture_one} alt="explanation of exercise one" />
-      <img  src={props.exercise.picture_two} alt="explanation of exercise two" />
- 
-   </div>
-  </div>
-</div>
+              <div className="description">
+                <i className="fas fa-running"></i> {props.exercise.description}
+              </div>
+              <div className="para">
+                <p> Level: {props.exercise.difficulty}</p>
+                <p> Type: {props.exercise.type}</p>
+                <p>Target: {props.exercise.muscle}</p>
+                <p> Equipment: {props.exercise.equipment}</p>
+              </div>
+              <div className="bottom-div">
+                <img className="last-img"
+                  src={props.exercise.picture_one}
+                  alt="explanation of exercise one"
+                />
+                <img className="last-img"
+                  src={props.exercise.picture_two}
+                  alt="explanation of exercise two"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Content>
     </>
   );
 };
