85793ed06ada4aa023939f79478f7cba0d7be6d7
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-09-12 10:24:56 +0400

    add ant to profile card

--- workout-tracker/src/views/UserPage/ProfileImage.js
+++ workout-tracker/src/views/UserPage/ProfileImage.js
@@ -1,61 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import 'antd/dist/antd.css';
+import { Avatar } from 'antd';
 
 const ProfileImage = ({username, image, weight, workouts}) => {
     return (
         <StyledImage>
             <div>
-                <img src={image} alt="" />
+                <Avatar size={64} icon="user" />
+                <p>{username}</p>
             </div>
-            <p>{username}</p>
             <section>
-              <div>Weight: {weight} kgs.</div>
-              <div>Workouts: {workouts}</div>
+              <span>Weight: {weight} kgs.</span>
+              <span>Workouts: {workouts}</span>
             </section>
         </StyledImage>
     )
 }
 
 const StyledImage = styled.div`
+    margin-top: 1rem;
     margin: 0 auto;
-    display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    border-bottom: 1px solid #696969;
-
+    border-bottom: 2px solid white;
     div {
-        border-radius: 50%;
-        border: 3px solid white;
-        width: 80px;
-        height: 80px;
-
-        img {
-            border-radius: inherit;
-            width: inherit;
-            height: inherit;
-        }
+        text-align: center;
     }
-
     p {
         text-align: center;
-        color: #2d2d2d;
+        color: white;
         font-size: inherit;
         font-weight: 600;
         margin-bottom: 0;
-        padding: 10px 30px;
+        padding: 5px 0;
     }
     section {
       display: flex;
       flex-direction: column;
-      align-items: flex-start;
-      padding-bottom: 1.5rem;
-
-
-      div {
-        height: auto;
-        width: auto;
-      }
+      align-items: center;
+      padding-bottom: 1rem;
+      background-color: inherit;
+      color: white;
+        span {
+            height: auto;
+            width: auto;
+        }
     }
 `
 
