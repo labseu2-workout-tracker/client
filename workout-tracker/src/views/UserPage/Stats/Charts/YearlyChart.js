3a0bb4c3ef4872134168d7c26d842e8fe27e3d2a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-12 22:12:53 +0200

    added functionality to add pur data from the database to the chart

--- workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
+++ workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
@@ -196,6 +196,10 @@ const { Meta } = Card;
 
 const StyledMonthlyChart = styled.div`
   border-radius: 0.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
 
   .apexcharts-toolbar {
     display: none;
@@ -248,6 +252,100 @@ class LineChart extends React.Component {
     };
   }
 
+    componentDidMount = () => {
+    this.props.fetchWorkouts();
+    this.props.fetchWorkoutsHistory();
+    let workoutNames = [];
+    let workouts = [];
+
+        this.props.workouts.map(workout => {
+          workoutNames.push(workout.workout_name);
+          workouts.push(workout);
+          return workout;
+        });
+
+            let year = new Date().getFullYear();
+            let first_day_year = new Date(year, 0, 1);
+            let last_day_year = new Date(year, 11, 31);
+
+            var getDaysArray = function(s, e) {
+              for (var a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
+                a.push(new Date(d));
+              }
+              return a;
+            };
+
+            let daylist = getDaysArray(first_day_year, last_day_year);
+            daylist.map(v => v.toISOString().slice(0, 10)).join("");
+
+            let daysInYear = [];
+
+            function formatDate(date) {
+              var d = new Date(date),
+                month = "" + (d.getMonth() + 1),
+                day = "" + d.getDate(),
+                year = d.getFullYear();
+
+              if (month.length < 2) month = "0" + month;
+              if (day.length < 2) day = "0" + day;
+
+              return [year, month, day].join("-");
+            }
+
+            for (let i = 0; i < daylist.length; i++) {
+              daysInYear.push(
+                formatDate(daylist[i])
+                  .split("-")
+                  .join("")
+              );
+            }
+
+            let userHistory = this.props.history;
+            let resultOfWeek = [];
+
+            for (let j = 0; j < daysInYear.length; j++) {
+              for (let i = 0; i < userHistory.length; i++) {
+                if (
+                  userHistory[i].session_start
+                    .match(/.{1,10}/g)[0]
+                    .split("-")
+                    .join("") === daysInYear[j]
+                ) {
+                  resultOfWeek.push(userHistory[i]);
+                }
+              }
+            }
+
+            let hashTable = {};
+
+            for (let j = 0; j < workouts.length; j++) {
+              hashTable[workouts[j].workout_name] = 0;
+            }
+
+            for (let i = 0; i < resultOfWeek.length; i++) {
+              for (let j = 0; j < workouts.length; j++) {
+                if (resultOfWeek[i].workout_id === workouts[j].id) {
+                  if (hashTable[workouts[j].workout_name]) {
+                    hashTable[workouts[j].workout_name] += 1;
+                  } else {
+                    hashTable[workouts[j].workout_name] = 1;
+                  }
+                }
+              }
+            }
+
+            let valuesForDataset = [];
+
+            for (var value in hashTable) {
+              valuesForDataset.push(hashTable[value]);
+            }
+
+            this.setState({
+              data: valuesForDataset,
+              categories: workoutNames
+            });
+  };
+
   render() {
     return (
       <StyledMonthlyChart style={{ backgroundColor: "#FC940C" }}>
@@ -263,6 +361,7 @@ class LineChart extends React.Component {
           series={[{ name: this.state.name, data: this.state.data }]}
           type="line"
           height="350"
+          width="700"
         />
       </StyledMonthlyChart>
     );
