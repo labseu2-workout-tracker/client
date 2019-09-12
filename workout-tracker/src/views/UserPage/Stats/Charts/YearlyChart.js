bd333e2a1ef0c8d138671db91c375a7b2dfc5e30
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-12 20:57:58 +0200

    import fetchWorkoutHistory

--- workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
+++ workout-tracker/src/views/UserPage/Stats/Charts/YearlyChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
 import { fetchWorkouts } from "../../../../store/actions/workoutsActions";
+import { fetchWorkoutsHistory } from "../../../../store/actions/historyActions";
 import { axiosWithAuth } from "../../../../store/axiosWithAuth";
 import { Card } from "antd";
 import { connect } from "react-redux";
