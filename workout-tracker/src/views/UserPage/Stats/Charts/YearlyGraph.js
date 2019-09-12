32d356a19f34055c98e22d8300047f6555a93f8f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-12 23:05:23 +0200

    added imports

---
+++ workout-tracker/src/views/UserPage/Stats/Charts/YearlyGraph.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { fetchWorkouts } from "../../../../store/actions/workoutsActions";
+import { fetchWorkoutsHistory } from "../../../../store/actions/historyActions";
+import { Card } from "antd";
+import { connect } from "react-redux";
+import ReactApexChart from "react-apexcharts";
+import styled from "styled-components";
