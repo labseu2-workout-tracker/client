b8e9a63c29ace836179d45b1f65bfec44c050811
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-12 23:06:05 +0200

    destructuring

--- workout-tracker/src/views/UserPage/Stats/Charts/YearlyGraph.js
+++ workout-tracker/src/views/UserPage/Stats/Charts/YearlyGraph.js
@@ -5,3 +5,5 @@ import { Card } from "antd";
 import { connect } from "react-redux";
 import ReactApexChart from "react-apexcharts";
 import styled from "styled-components";
+
+const { Meta } = Card;
