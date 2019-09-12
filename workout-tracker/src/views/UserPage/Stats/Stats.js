0a13c876996396d83466395fc0733f3a294d252d
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-12 15:50:57 +0200

    new card for chart two

--- workout-tracker/src/views/UserPage/Stats/Stats.js
+++ workout-tracker/src/views/UserPage/Stats/Stats.js
@@ -12,16 +12,16 @@ const { Meta } = Card;
 
 const StyledStats = styled.div`
   overflow: hidden;
-  
+
   .chart {
     width: 100%;
     border: 0;
     display: flex;
     position: relative;
-    font-size: .875rem;
+    font-size: 0.875rem;
     min-width: 0;
     word-wrap: break-word;
-    background: #FFF;
+    background: #fff;
     box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.14);
     margin-top: 30px;
     border-radius: 6px;
@@ -30,19 +30,19 @@ const StyledStats = styled.div`
   }
 
   .chart-one {
-    background-color: #FFF;
+    background-color: #fff;
   }
 
   .chart-two {
-    background-color: #FFF;
+    background-color: #fff;
   }
 
   .chart-three {
-    background-color: #FFF;
+    background-color: #fff;
   }
 
   .calendar {
-    background-color: #FFF;
+    background-color: #fff;
   }
 
   .calendar {
@@ -50,18 +50,16 @@ const StyledStats = styled.div`
     border: 0;
     display: flex;
     position: relative;
-    font-size: .875rem;
+    font-size: 0.875rem;
     min-width: 0;
     word-wrap: break-word;
-    background: #FFF;
+    background: #fff;
     box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.14);
     margin-top: 30px;
     border-radius: 6px;
     margin-bottom: 30px;
     flex-direction: column;
   }
-
-
 `;
 
 class Stats extends React.Component {
@@ -74,38 +72,31 @@ class Stats extends React.Component {
       <StyledStats>
         <Row>
           <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-          <Card
-    hoverable
-    className="chart chart-one"
-    cover={<WeeklyChart/>}
-  >
-    <Meta title="Weekly Result" description="www.instagram.com" />
-  </Card>
-          </Col>
-          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            <Card 
-            className="chart chart-two"
-            title="Monthly Result" 
-            >
-              <MonthlyChart />
+            <Card hoverable className="chart chart-one" cover={<WeeklyChart />}>
+              <Meta title="Weekly Result" description="www.instagram.com" />
             </Card>
           </Col>
-          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
-            <Card 
-            className="chart chart-three"
-            title="Yearly Result" 
+
+          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <Card
+              hoverable
+              className="chart chart-two"
+              cover={<MonthlyChart />}
             >
+              <Meta title="Monthly Result" description="www.instagram.com" />
+            </Card>
+          </Col>
+
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            <Card className="chart chart-three" title="Yearly Result">
               <YearlyChart />
             </Card>
           </Col>
         </Row>
         <Row>
-        <Card
-            className="calendar"
-            title="Calendar" 
-            >
-          <WorkoutCalendar />
-            </Card>
+          <Card className="calendar" title="Calendar">
+            <WorkoutCalendar />
+          </Card>
         </Row>
         <Row></Row>
         <Row>
