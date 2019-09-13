f9af77b794ae6326e0572a46ce528094334d3295
Duduiala Ioana <becheru93@gmail.com>
2019-09-13 08:15:32 +0100

    latest changes

--- workout-tracker/src/views/UserPage/MyWorkouts/MyWorkouts.js
+++ workout-tracker/src/views/UserPage/MyWorkouts/MyWorkouts.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchWorkoutDetails, deleteWorkout } from '../../../store/actions/workoutsActions';
-<<<<<<< HEAD
 import {Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import { Empty } from 'antd';
-=======
-import { Link } from 'react-router-dom';
 import WorkoutPage from '../../customWorkout/WorkoutPage'
->>>>>>> cdf9a6b3ddf3124ba1e3d4f9da397ebe6538cd88
+import { Empty } from 'antd';
 // import styled from 'styled-components';
 
 // const StyledWorkoutView = styled.div``;
@@ -39,19 +34,11 @@ class WorkoutView extends React.Component {
             <p onClick={() => this.props.deleteWorkout(workout.id)} className='btn'>
               Delete Workout
               </p>
-<<<<<<< HEAD
         </div> 
       })) : (<Link to='/Workouts'><Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={'Add some workouts'}/></Link>)}
     </div>
+     <WorkoutPage /> 
             </div>;
-=======
-          </div>
-        })) : <h1>You have no workouts yet</h1>}
-      </div>
-      <WorkoutPage /> 
-     
-    </div>;
->>>>>>> cdf9a6b3ddf3124ba1e3d4f9da397ebe6538cd88
   }
 }
 
--- workout-tracker/src/views/UserPage/Stats/UserHistory/UserHistory.js
+++ workout-tracker/src/views/UserPage/Stats/UserHistory/UserHistory.js
@@ -11,7 +11,7 @@ class SessionHistory extends React.Component {
     super(props);
     this.state = {
       minValue: 0,
-      maxValue: 6
+      maxValue: 2
     };
   }
 
@@ -25,12 +25,12 @@ class SessionHistory extends React.Component {
     if (value <= 1) {
       this.setState({
         minValue: 0,
-        maxValue: 6
+        maxValue: 2
       });
     } else {
       this.setState({
-        minValue: value * 6 - 6,
-        maxValue: value * 6
+        minValue: (value * 2) - 2,
+        maxValue: value * 2
       });
     }
   };
@@ -47,72 +47,70 @@ class SessionHistory extends React.Component {
         <StyledUserHistory>
           {history.length !== 0 ? (
             <div>
-              <div className="ordered-list">
-                {history
-                  .slice(this.state.minValue, this.state.maxValue)
-                  .map(session => {
-                    const date1 = session.session_start;
-                    const date2 = session.session_end;
+              {history
+                .slice(this.state.minValue, this.state.maxValue)
+                .map(session => {
+                  const date1 = session.session_start;
+                  const date2 = session.session_end;
 
-                    // Extract starting point
-                    const startingPoint =
-                      date1 === null ? "00:00:00" : date1.slice(11, 17);
-                    const endPoint =
-                      date2 === null ? "00:00:00" : date2.slice(11, 17);
+                  // Extract starting point
+                  const startingPoint =
+                    date1 === null ? "00:00:00" : date1.slice(11, 17);
+                  const endPoint =
+                    date2 === null ? "00:00:00" : date2.slice(11, 17);
 
-                    function pluralize(hours) {
-                      return hours <= 1 ? "hour" : "hours";
+                  function pluralize(hours) {
+                    return hours <= 1 ? "hour" : "hours";
+                  }
+
+                  function diff(start, end) {
+                    start = start.split(":");
+                    end = end.split(":");
+                    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+                    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+                    var diff = endDate.getTime() - startDate.getTime();
+                    var hours = Math.floor(diff / 1000 / 60 / 60);
+                    diff -= hours * 1000 * 60 * 60;
+                    var minutes = Math.floor(diff / 1000 / 60);
+
+                    // If using time pickers with 24 hours format, add the below line get exact hours
+                    if (hours <= 0) {
+                      return `${minutes} minutes`;
                     }
+                    return `${hours} ${pluralize(hours)} ${minutes} minutes`;
+                  }
 
-                    function diff(start, end) {
-                      start = start.split(":");
-                      end = end.split(":");
-                      var startDate = new Date(0, 0, 0, start[0], start[1], 0);
-                      var endDate = new Date(0, 0, 0, end[0], end[1], 0);
-                      var diff = endDate.getTime() - startDate.getTime();
-                      var hours = Math.floor(diff / 1000 / 60 / 60);
-                      diff -= hours * 1000 * 60 * 60;
-                      var minutes = Math.floor(diff / 1000 / 60);
-
-                      // If using time pickers with 24 hours format, add the below line get exact hours
-                      if (hours <= 0) {
-                        return `${minutes} minutes`;
-                      }
-                      return `${hours} ${pluralize(hours)} ${minutes} minutes`;
-                    }
-
-                    return (
-                      <ol key={session.id}>
-                        <li>
-                          <p>
-                            <strong>Session Started : </strong>
-                            {session.session_start.slice(0, 10)}
-                          </p>
-                          <p>
-                            <strong>Workout Name : </strong>
-                            {workouts === undefined ? (
-                              <h2>Loadin workouts...</h2>
-                            ) : (
-                              workouts.map(item => {
-                                if (session.workout_id === item.id) {
-                                  return item.workout_name;
-                                }
-                                return null;
-                              })
-                            )}
-                          </p>
-                          <p>
-                            <strong>Duration : </strong>
-                            {diff(startingPoint, endPoint)}
-                          </p>
-                        </li>
-                      </ol>
-                    );
-                  })}
-              </div>
+                  return (
+                    <ol key={session.id}>
+                      <li>
+                        <p>
+                          <strong>Session Started : </strong>
+                          {session.session_start.slice(0, 10)}
+                        </p>
+                        <p>
+                          <strong>Workout Name : </strong>
+                          {workouts === undefined ? (
+                            <p>Loadin workouts...</p>
+                          ) : (
+                            workouts.map(item => {
+                              if (session.workout_id === item.id) {
+                                return item.workout_name;
+                              }
+                              return null;
+                            })
+                          )}
+                        </p>
+                        <p>
+                          <strong>Duration : </strong>
+                          {diff(startingPoint, endPoint)}
+                        </p>
+                      </li>
+                    </ol>
+                  );
+                })}
               <Pagination
                 defaultCurrent={1}
-                defaultPageSize={6}
+                defaultPageSize={2}
                 onChange={this.handlePagination}
                 total={this.props.history.length}
               />
@@ -142,14 +140,11 @@ export default connect(
 
 
 const StyledUserHistory = styled.div`
-
-  width: 100%;
+  width: 50%;
   margin: 0 auto; 
  
   ol {
     padding: 10px;
-    width: 50%;
-    margin: 0;
   }
 
   h2 {
@@ -180,4 +175,4 @@ const StyledUserHistory = styled.div`
     font-size: 0.9rem;
     padding: 10px;
   }
-`
+`;
