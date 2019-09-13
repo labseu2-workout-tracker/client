289b0cc4df1b8508d1bbe7b1b7568cc0bafd0a83
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-13 12:38:22 +0200

    change

--- workout-tracker/src/components/Layout/Layout.js
+++ workout-tracker/src/components/Layout/Layout.js
@@ -1,50 +1,46 @@
-import React from 'react';
+import React from "react";
 import "antd/dist/antd.css";
 import { Layout } from "antd";
 import Auth from "../../auth/auth";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const { Header, Content, Sider } = Layout;
 
 class MainLayout extends React.Component {
-
   render() {
     return (
       <StyledContainer>
-        {!Auth.isAuthenticated() ? <Header>{this.props.header}</Header> : '' }
+        {!Auth.isAuthenticated() ? <Header>{this.props.header}</Header> : ""}
         {this.props.mobileNav}
-        <div className='content-container'>
-        {Auth.isAuthenticated() ? <Sider>{this.props.sider}</Sider> : '' }
-        <Content>{this.props.routes}</Content>
+        <div className="content-container">
+          {Auth.isAuthenticated() ? <Sider>{this.props.sider}</Sider> : ""}
+          <Content>{this.props.routes}</Content>
         </div>
-
       </StyledContainer>
-    )
+    );
   }
 }
 
 const StyledContainer = styled.section`
   display: flex;
   flex-direction: column;
-
-  .content-container{
+  .content-container {
     display: flex;
-    flex-direction: row
+    flex-direction: row;
   }
   .ant-layout-sider {
     padding: 0.5rem;
     background-color: #001529;
-
-  .ant-layout-content {
-    margin: 0;
-    padding: 0;
-    min-height: 100vh;
+    .ant-layout-content {
+      margin: 0;
+      padding: 0;
+      min-height: 100vh;
+    }
+    .nav-items {
+      padding: 24px 0;
+    }
   }
-  .nav-items {
-    padding: 24px 0;
-  }
-}
-`
+`;
 
 export default MainLayout;
 
