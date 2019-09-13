c046a630a0da5461ee3b8e3a7687efb76f5d6312
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-09-13 13:02:17 +0400

    removed commented code

--- workout-tracker/src/components/Layout/Layout.js
+++ workout-tracker/src/components/Layout/Layout.js
@@ -54,20 +54,3 @@ const StyledContainer = styled.section`
 `
 
 export default MainLayout;
-
-// import React from 'react';
-// import "antd/dist/antd.css";
-// import { Layout } from "antd";
-// import './Layout.css';
-
-// const { Header, Content } = Layout;
-
-// const MainLayout = props => (
-//   <>
-//     <Header className="main-header">{props.header}</Header>
-//     {props.mobileNav}
-//     <Content className="content">{props.routes}</Content>
-//   </>
-// );
-
-// export default MainLayout;
