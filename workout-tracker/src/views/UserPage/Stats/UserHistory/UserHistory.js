72b5d8e7495491dc6326e83a76bd18026c103736
Duduiala Ioana <becheru93@gmail.com>
2019-09-16 15:48:53 +0100

    loader resized

--- workout-tracker/src/views/UserPage/Stats/UserHistory/UserHistory.js
+++ workout-tracker/src/views/UserPage/Stats/UserHistory/UserHistory.js
@@ -135,11 +135,11 @@ export default connect(mapStateToProps)(SessionHistory);
 
 
 const Loader = styled.div`
-border: 16px solid #f3f3f3; /* Light grey */
-border-top: 16px solid #3498db; /* Blue */
+border: 8px solid #f3f3f3; /* Light grey */
+border-top: 8px solid #3498db; /* Blue */
 border-radius: 50%;
-width: 120px;
-height: 120px;
+width: 50px;
+height: 50px;
 animation: spin 2s linear infinite;
 
 
