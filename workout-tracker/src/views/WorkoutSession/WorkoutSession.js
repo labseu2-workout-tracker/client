960fe430516e1a57822dc5bb943bfeef9fe9a494
Talent Antonio <antonio.talenty@gmail.com>
2019-09-17 22:46:36 +0100

    desktop view done

--- workout-tracker/src/views/WorkoutSession/WorkoutSession.js
+++ workout-tracker/src/views/WorkoutSession/WorkoutSession.js
@@ -78,29 +78,29 @@ class WorkoutSession extends React.Component {
             type="inner"
           >
             <div style={{ background: '#ECECEC', padding: '30px' }}>
-              <Row gutter={16}>
+              {/* <Row gutter={16}>
                 <Col // image and details
                   span={8}
-                >
-                  <Card
-                    //  style={{ width: 240 }}
-                    title={this.props.currentExercise[0].exercise_name}
-                  >
-                    <img
-                      bordered={false}
-                      style={{ width: 200 }}
-                      alt="Exercise explanation"
-                      src={this.props.currentExercise[0].picture_one}
-                    />
-                    <div>
-                      <img
-                        style={{ width: 200 }}
-                        alt="Exercise explanation"
-                        src={this.props.currentExercise[0].picture_two}
-                      />
-                    </div>
-                  </Card>
-                  {/* <Card
+                > */}
+              <Card
+                //  style={{ width: 240 }}
+                title={this.props.currentExercise[0].exercise_name}
+              >
+                <img
+                  bordered={false}
+                  style={{ width: 200 }}
+                  alt="Exercise explanation"
+                  src={this.props.currentExercise[0].picture_one}
+                />
+                <div>
+                  <img
+                    style={{ width: 200 }}
+                    alt="Exercise explanation"
+                    src={this.props.currentExercise[0].picture_two}
+                  />
+                </div>
+              </Card>
+              {/* <Card
                     title={this.props.currentExercise[0].exercise_name}
                     bordered={false}
                     hoverable
@@ -112,130 +112,146 @@ class WorkoutSession extends React.Component {
                       />
                     }
                   ></Card> */}
-                </Col>
+              {/* </Col>
                 <Col // Stats
                   span={8}
-                >
-                  <Card title="stats" bordered={false}>
-                    <Card
-                      title={this.props.currentExercise[0].exercise_name}
-                      actions={[
-                        <Statistic
-                          title="Sets to complete"
-                          prefix={<Icon type="unordered-list" />}
-                          style={{ cursor: 'default' }}
-                          value={this.props.currentExercise.length}
-                        />,
-                        <Statistic
-                          title={
-                            this.props.currentExercise[0].reps
-                              ? 'Repetitions'
-                              : 'Duration'
+                > */}
+              <Card
+                title="stats"
+                bordered={false}
+                style={{ display: 'flex', flexDirection: 'column' }}
+              >
+                <Card
+                  title={this.props.currentExercise[0].exercise_name}
+                  actions={[
+                    <Statistic
+                      title="Sets to complete"
+                      prefix={<Icon type="list" />}
+                      style={{ cursor: 'default' }}
+                      value={this.props.currentExercise.length}
+                    />,
+                    <Statistic
+                      title={
+                        this.props.currentExercise[0].reps
+                          ? 'Repetitions'
+                          : 'Duration'
+                      }
+                      prefix={<Icon type="sync" spin />}
+                      value={
+                        this.props.currentExercise[0].reps ||
+                        (this.props.currentExercise[0].duration
+                          ? this.props.currentExercise[0].duration
+                          : '20seconds')
+                      }
+                      style={{ cursor: 'default' }}
+                    />,
+                    <Statistic
+                      title="Next Exercise"
+                      prefix={
+                        <Icon
+                          onClick={() =>
+                            this.props.finishExercise(
+                              this.props.currentExercise[0].id
+                            )
                           }
-                          prefix={<Icon type="sync" spin />}
-                          value={
-                            this.props.currentExercise[0].reps ||
-                            (this.props.currentExercise[0].duration
-                              ? this.props.currentExercise[0].duration
-                              : '20seconds')
-                          }
-                          style={{ cursor: 'default' }}
-                        />,
-                        <Statistic
-                          title="Next Exercise"
-                          prefix={
-                            <Icon
-                              onClick={() =>
-                                this.props.finishExercise(
-                                  this.props.currentExercise[0].id
-                                )
-                              }
-                              type="double-right"
-                            />
-                          }
-                          style={{ cursor: 'default' }}
-                          value=" "
+                          type="fast-forward"
                         />
-                      ]}
-                    >
-                      <Card.Meta
-                        description={`Equipment Needed: ${this.props.currentExercise[0].equipment}`}
-                      />
-                    </Card>
-                   
-                    <Button type="danger" onClick={this.endWorkout}>
-                      Finish Workout
-                    </Button>
-                  </Card>
-                </Col>
+                      }
+                      style={{ cursor: 'default' }}
+                      value=" "
+                    />
+                  ]}
+                >
+                  <Card.Meta
+                    description={`Equipment Needed: ${this.props.currentExercise[0].equipment}`}
+                  />
+                </Card>
+
+                <div style={{margin:20, display:'flex', justifyContent:'center'}}>
+                  <Button type="danger" onClick={this.endWorkout}>
+                    Finish Workout
+                  </Button>
+                </div>
+              </Card>
+              {/* </Col>
                 <Col // Watch
                   span={8}
-                >
-                  <Card title="Watch" bordered={false}>
-                    <Watch />
-                  </Card>
-                </Col>
-              </Row>
+                > */}
+              <Card
+                title="Watch"
+                bordered={false}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignContent: 'spaceAround'
+                }}
+              >
+                <Watch />
+              </Card>
+              {/* </Col> */}
+              {/* </Row> */}
             </div>
           </Card>
           <Card //Bottom Card with Exer & Instructions
-            style={{ marginTop: 16  }}
+            style={{ marginTop: 16 }}
             type="outer"
           >
-            <div style={{ background: '#ECECEC', padding: '30px', display:'flex', flexDirection:'row'}}>
+            <div
+              style={{
+                background: '#ECECEC',
+                padding: '30px',
+                display: 'flex',
+                flexDirection: 'row'
+              }}
+            >
               {/* <Row gutter={16}>
                 <Col span={12}> */}
-                  <Card //Instructions bar
-                    title="Instructions"
-                    bordered={false}
-                  >
-                    <Card bordered={false}>
-                      <Alert
-                        message="Instructions"
-                        description={this.props.currentExercise[0].description}
-                        type="info"
-                      />
-                      {/* {`${this.state.initial} ==> ${this.props.currentExercise.length} ===> ${this.props.allExercises.length}`} */}
-                    </Card>
-                  </Card>
-                {/* </Col> */}
-                {/* <Col span={12}> */}
-                  <Card // Excercise List
-                    title="Excercise List"
-                    bordered={false}
-                  >
-                    <List
-                      size="small"
-                      header={<h3>Choose Exercises</h3>}
-                      dataSource={this.props.allExercises
-                        .reduce((acc, current) => {
-                          const x = acc.find(
-                            (item) =>
-                              item.exercise_name === current.exercise_name
-                          );
-                          if (!x) {
-                            return acc.concat([current]);
-                          } else {
-                            return acc;
-                          }
-                        }, [])
-                        .map((exercise) => exercise.exercise_name)}
-                      renderItem={(item) => (
-                        <List.Item
-                          onClick={() => this.props.chooseExercise(item)}
-                        >
-                          {
-                            <Button
-                            style={{textAlign:"left"}}
-                            type="link" block>
-                              {item}
-                            </Button>
-                          }
-                        </List.Item>
-                      )}
-                    />
-                  </Card>
-                {/* </Col> */}
+              <Card //Instructions bar
+                title="Instructions"
+                bordered={false}
+              >
+                <Card bordered={false}>
+                  <Alert
+                    message="Instructions"
+                    description={this.props.currentExercise[0].description}
+                    type="info"
+                  />
+                  {/* {`${this.state.initial} ==> ${this.props.currentExercise.length} ===> ${this.props.allExercises.length}`} */}
+                </Card>
+              </Card>
+              {/* </Col> */}
+              {/* <Col span={12}> */}
+              <Card // Excercise List
+                title="Excercise List"
+                bordered={false}
+              >
+                <List
+                  size="small"
+                  header={<h3>Choose Exercises</h3>}
+                  dataSource={this.props.allExercises
+                    .reduce((acc, current) => {
+                      const x = acc.find(
+                        (item) => item.exercise_name === current.exercise_name
+                      );
+                      if (!x) {
+                        return acc.concat([current]);
+                      } else {
+                        return acc;
+                      }
+                    }, [])
+                    .map((exercise) => exercise.exercise_name)}
+                  renderItem={(item) => (
+                    <List.Item onClick={() => this.props.chooseExercise(item)}>
+                      {
+                        <Button style={{ textAlign: 'left' }} type="link" block>
+                          {item}
+                        </Button>
+                      }
+                    </List.Item>
+                  )}
+                />
+              </Card>
+              {/* </Col> */}
               {/* </Row> */}
             </div>
           </Card>
