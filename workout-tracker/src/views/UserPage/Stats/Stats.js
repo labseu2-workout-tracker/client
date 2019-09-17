80a79779029b6a36eed0d43285b6339953b22139
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-17 18:23:35 +0200

    delete comments

--- workout-tracker/src/views/UserPage/Stats/Stats.js
+++ workout-tracker/src/views/UserPage/Stats/Stats.js
@@ -120,25 +120,3 @@ export default connect(
   null,
   { fetchWorkouts, fetchWorkoutsHistory }
 )(Stats);
-
-{/* <Slider className="slider-wrapper" autoplay={1000}>
-{content.map((item, index) => (
-  <div
-    key={index}
-    className="slider-content"
-    style={{ background: `url('${item.image}') no-repeat center center` }}
-  >
-    <div className="inner">
-      <h1>{item.title}</h1>
-      <p>{item.description}</p>
-      <button>{item.button}</button>
-    </div>
-    <section>
-      <img src={item.userProfile} alt={item.user} />
-      <span>
-        Posted by <strong>{item.user}</strong>
-      </span>
-    </section>
-  </div>
-))}
-</Slider> */}
