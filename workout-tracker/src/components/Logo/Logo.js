d6e2b7fa5c5f919f6bdc8a51f90ab9df1b8657cd
Talent Antonio <antonio.talenty@gmail.com>
2019-09-18 12:08:08 +0100

    deleted old logo

--- workout-tracker/src/components/Logo/Logo.js
+++ workout-tracker/src/components/Logo/Logo.js
@@ -3,6 +3,11 @@ import logoImage from '../../assets/images/logo-svg-white.svg'
 import './Logo.css';
 import { Link } from 'react-router-dom';
 
-const logo = props => <div className="logo-container"><Link to='/workouts'><img className="image" src={logoImage} alt="logo" /></Link></div>
+const logo = props => <div className="logo-container">
+    <Link to='/workouts'>
+    <h3 id="newstuff">
+            Be<span>Fit</span>
+          </h3>
+    </Link></div>
 
 export default logo;
