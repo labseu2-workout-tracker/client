ad54c2839778e3b7c8ea1c09bba9e37741a7bb47
Becheru888 <becheru93@gmail.com>
2019-09-26 14:15:40 +0100

    last changes

--- workout-tracker/src/components/Layout/Layout.js
+++ workout-tracker/src/components/Layout/Layout.js
@@ -30,37 +30,45 @@ class MainLayout extends React.Component {
           <Content>
             {this.props.routes}
             <Footer>
-              <FooterContent style={{ display: "flex", flexDirection: "row", justifyContent: 'space-between'}} >
-              <div>
-                <h4>Our statement</h4>
-                <p>
-                  If you can make it that far,<br></br> what's stopping you from
-                  one more mile or one more set of reps?
-                </p>
-              </div>
-              <div>
-                <h4>Contact us</h4>
-                <p>
-                  <a href="#">befit@tracker.com</a>
-                </p>
-              </div>
-              <div style={{display:'flex', flexDirection:'column'}}>
-                 <h4>The Team Behind</h4>
-                 <a>Shaun Carmody TL</a>
-                 <a href='https://github.com/Becheru888'>Remi Becheru FS</a>        
-                 <a href='https://github.com/benjamingrabow '>Benjamin Grabow FS</a>
-                 <a>Matt Locklin FS</a>
-                 <a href='https://github.com/hyetigran'>Tigran Asriyan FS</a>
-                 <a>Yusuf Abdulkarim FS</a>
-                 <a>Kelechi Ogbonna FS</a>
-                 <a>Talent Antonio FS</a>
-                 <a>Wasiu Idowu FS</a>               
-              </div>
+              <FooterContent
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between"
+                }}
+              >
+                <div>
+                  <h4>Our statement</h4>
+                  <p>
+                    If you can make it that far,<br></br> what's stopping you
+                    from one more mile or one more set of reps?
+                  </p>
+                </div>
+                <div>
+                  <h4>Contact us</h4>
+                  <p>
+                    <a href="#">befit@tracker.com</a>
+                  </p>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <h4>The Team Behind</h4>
+                  <a>Shaun Carmody TL</a>
+                  <a href="https://github.com/Becheru888">Remi Becheru FS</a>
+                  <a href="https://github.com/benjamingrabow ">
+                    Benjamin Grabow FS
+                  </a>
+                  <a>Matt Locklin FS</a>
+                  <a href="https://github.com/hyetigran">Tigran Asriyan FS</a>
+                  <a>Yusuf Abdulkarim FS</a>
+                  <a>Kelechi Ogbonna FS</a>
+                  <a>Talent Antonio FS</a>
+                  <a>Wasiu Idowu FS</a>
+                </div>
               </FooterContent>
               <Copy>
-                
                 <p>
-                <span>© Copyright {new Date().getFullYear()}</span><br></br>
+                  <span>© Copyright {new Date().getFullYear()}</span>
+                  <br></br>
                   All rights reserved. Powered by <a href="#!">BeFit</a>
                 </p>
               </Copy>
@@ -72,19 +80,20 @@ class MainLayout extends React.Component {
   }
 }
 
+
+
 const Copy = styled.div`
-p {
-  text-align:center;
-  padding:0;
-}
-`
-
-const FooterContent = styled.div `
-    p{
-    padding:0px;
+  p {
+    text-align: center;
+    padding: 0;
   }
+`;
 
-`
+const FooterContent = styled.div`
+  p {
+    padding: 0px;
+  }
+`;
 
 const StyledContainer = styled.section`
   display: flex;
@@ -100,7 +109,7 @@ const StyledContainer = styled.section`
 
   .ant-layout-sider-children {
     display: flex;
-    flex-direction: column
+    flex-direction: column;
   }
 
   .ant-layout-content {
