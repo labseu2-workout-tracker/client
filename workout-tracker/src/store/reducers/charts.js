8d6a9c2adfc26fc6202f00f34cd38469078b81ce
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-26 15:24:00 +0200

    add case for calculating weekly chart

--- workout-tracker/src/store/reducers/charts.js
+++ workout-tracker/src/store/reducers/charts.js
@@ -7,6 +7,106 @@ const initialState = {
 const charts = (state = initialState, action) => {
   switch (action.type) {
     case type.CALCULATE_WEEKLY_CHART:
+        let workoutNames = [];
+        let workouts = [];
+    
+        action.workouts.map(workout => {
+          workoutNames.push(workout.workout_name);
+          workouts.push(workout);
+          return workout;
+        });
+    
+        let weekMap = [6, 0, 1, 2, 3, 4, 5];
+    
+        function startAndEndOfWeek(date) {
+          let now = new Date(date);
+          now.setHours(0, 0, 0, 0);
+          let monday = new Date(now);
+          monday.setDate(monday.getDate() - weekMap[monday.getDay()]);
+          let sunday = new Date(now);
+          sunday.setDate(sunday.getDate() - weekMap[sunday.getDay()] + 6);
+          sunday.setHours(23, 59, 59, 999);
+          return [monday, sunday];
+        }
+    
+        let startAndEndWeek = startAndEndOfWeek(new Date());
+    
+        let getDaysArray = function(s, e) {
+          for (var a = [], d = s; d <= e; d.setDate(d.getDate() + 1)) {
+            a.push(new Date(d));
+          }
+          return a;
+        };
+        let daylist = getDaysArray(startAndEndWeek[0], startAndEndWeek[1]);
+        daylist.map(v => v.toISOString().slice(0, 10)).join("");
+    
+        let daysInWeek = [];
+    
+        function formatDate(date) {
+          let d = new Date(date),
+            month = "" + (d.getMonth() + 1),
+            day = "" + d.getDate(),
+            year = d.getFullYear();
+    
+          if (month.length < 2) month = "0" + month;
+          if (day.length < 2) day = "0" + day;
+    
+          return [year, month, day].join("-");
+        }
+    
+        for (let i = 0; i < daylist.length; i++) {
+          daysInWeek.push(
+            formatDate(daylist[i])
+              .split("-")
+              .join("")
+          );
+        }
+    
+        let userHistory = action.history;
+        let resultOfWeek = [];
+    
+        for (let j = 0; j < daysInWeek.length; j++) {
+          for (let i = 0; i < userHistory.length; i++) {
+            if (
+              userHistory[i].session_start
+                .match(/.{1,10}/g)[0]
+                .split("-")
+                .join("") === daysInWeek[j]
+            ) {
+              resultOfWeek.push(userHistory[i]);
+            }
+          }
+        }
+    
+        let hashTable = {};
+    
+        for (let j = 0; j < workouts.length; j++) {
+          hashTable[workouts[j].workout_name] = 0;
+        }
+    
+        for (let i = 0; i < resultOfWeek.length; i++) {
+          for (let j = 0; j < workouts.length; j++) {
+            if (resultOfWeek[i].workout_id === workouts[j].id) {
+              if (hashTable[workouts[j].workout_name]) {
+                hashTable[workouts[j].workout_name] += 1;
+              } else {
+                hashTable[workouts[j].workout_name] = 1;
+              }
+            }
+          }
+        }
+    
+        let valuesForDataset = [];
+    
+        for (var value in hashTable) {
+          valuesForDataset.push(hashTable[value]);
+        }
+    
+        // this.setState({
+        //   data: valuesForDataset,
+        //   labels: workoutNames
+        // });
+
       return {
         ...state,
       };
