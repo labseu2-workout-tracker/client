f5fb65ac3e0fb42b79018babea3d10f3c8aab056
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-26 15:45:53 +0200

    create calculateMonthlyChart action

--- workout-tracker/src/store/actions/chartActions.js
+++ workout-tracker/src/store/actions/chartActions.js
@@ -5,3 +5,7 @@ export const CALCULATE_YEARLY_CHART = "CALCULATE_YEARLY_CHART";
 export const calculateWeeklyChart = (history, workouts) => {
   return { type: CALCULATE_WEEKLY_CHART, history: history, workouts: workouts };
 };
+
+export const calculateMonthlyChart = (history, workouts) => {
+  return { type: CALCULATE_MONTHLY_CHART, history: history, workouts: workouts };
+};
