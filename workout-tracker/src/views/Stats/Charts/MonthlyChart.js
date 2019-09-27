3b436cada2cf75f49fa69138af465f0fb61b2f18
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-09-27 10:45:48 +0200

    import modal and button from antd

--- workout-tracker/src/views/Stats/Charts/MonthlyChart.js
+++ workout-tracker/src/views/Stats/Charts/MonthlyChart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Pie } from "react-chartjs-2";
-import { Card } from "antd";
+import { Modal, Button, Card } from "antd";
 
 const { Meta } = Card;
 
